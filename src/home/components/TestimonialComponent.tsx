0d5c0b8b7e159dc35128feb8c355fbe68d596a36
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-10-24 16:12:37 +0530

    Wip: Aligned the testimonial section and add heading for the testimonial for this purpose

--- src/home/components/TestimonialComponent.tsx
+++ src/home/components/TestimonialComponent.tsx
@@ -2,11 +2,18 @@
 
 const TestimonialComponent = () => {
     return (
-        <div className="flex flex-wrap items-center justify-center border-2 border-red-500">
-            <TestimonialBoxComponent></TestimonialBoxComponent>
-            <TestimonialBoxComponent></TestimonialBoxComponent>
-            <TestimonialBoxComponent></TestimonialBoxComponent>
-            <TestimonialBoxComponent></TestimonialBoxComponent>
+        <div className="mt-20 flex flex-col flex-wrap items-center justify-center">
+            <div className="mx-auto max-w-screen-sm">
+                <h2 className="mb-4 text-center text-4xl tracking-tight font-extrabold text-white">Testimonials</h2>
+                <p className="mb-8 text-center font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+            </div> 
+            <div className="flex flex-wrap items-center justify-center">
+                <TestimonialBoxComponent></TestimonialBoxComponent>
+                <TestimonialBoxComponent></TestimonialBoxComponent>
+                <TestimonialBoxComponent></TestimonialBoxComponent>
+                <TestimonialBoxComponent></TestimonialBoxComponent>
+                
+            </div>
         </div>
     )
 }
@@ -15,7 +22,7 @@ const TestimonialComponent = () => {
 // Here comes the single component for the testimonial component
 const TestimonialBoxComponent = () => {
     return (
-        <div className="w-1/2 pl-10 mb-8 border border-gray-200 rounded-xl shadow-sm border-gray-700 md:mb-12 bg-gray-800">
+        <div className="w-1/3 ml-10 mb-8 border border-gray-200 rounded-xl shadow-sm border-gray-700 md:mb-12 bg-gray-800">
             <figure className="flex flex-col items-center justify-center p-8 text-center  border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e bg-gray-800 border-gray-700">
                 <blockquote className="max-w-2xl mx-auto mb-4 text-gray-300 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-white">Very easy this was to integrate</h3>
