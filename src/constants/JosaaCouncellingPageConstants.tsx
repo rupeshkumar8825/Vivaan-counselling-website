2e2121a7eb7cb6df1d069a590eb52f10c06f0ebb
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-10-29 20:27:07 +0530

    Wip : Add the constant file for the josaa councelling page

---
+++ src/constants/JosaaCouncellingPageConstants.tsx
@@ -0,0 +1,25 @@
+// here we will store the constants related to the josaa counselling page 
+
+
+// OVERVIEW  COMPONENT FOR JOSAA COUNsELLING PAGE 
+// constant for storing the submenu names which we have to show in dropdown
+export const JosaaCounsellingPageSubMenuList : Array<string> = [
+    "What is JOSAA Counselling?",
+    "JOSAA Counselling Documents",
+    "JOSAA Counselling Application Process",
+    "JOSAA Counselling Fee",
+    "JOSAA Counselling Cutoff",
+    "JOSAA Counselling Online Reporting",
+    "JOSAA Counselling Physical Reporting",
+]
+
+
+export const JosaaCounsellingPageRoutesList : Array<string> = [
+    "/counselling/josaa/introduction", 
+    "/counselling/josaa/documents", 
+    "/counselling/josaa/application-process", 
+    "/counselling/josaa/fee", 
+    "/counselling/josaa/cutoff", 
+    "/counselling/josaa/online-reporting",
+    "/counselling/josaa/physical-reporting",
+]
