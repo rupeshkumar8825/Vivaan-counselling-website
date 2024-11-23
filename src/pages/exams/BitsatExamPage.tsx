316251be67ff0bb539c22a19ca938d381b21e755
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-11-23 13:14:50 +0530

    Feature: Add bitsat exam home page

--- src/pages/exams/BitsatExamPage.tsx
+++ src/pages/exams/BitsatExamPage.tsx
@@ -3,7 +3,7 @@ import OverviewSectionComponent from "../../home/components/OverviewSectionCompo
 import VideoCard from "../../components/VideoCardComponent"
 import BlogComponent from "../../components/blog/BlogComponent"
 import { useNavigate } from "react-router-dom"
-import { JeeAdvanceContentList, JeeAdvanceExamPageButtonCodesList, JeeAdvanceExamPageButtonWidthList, JeeAdvanceExamRoutesList, JeeAdvanceExamsButtonNameList, JeeAdvanceExamSubMenuList, JeeAdvanceHeadingIdList, JeeAdvanceHeadingList, JeeAdvanceTableOfContentList, JeeAdvanceVideoCardTitleList, JeeAdvanceVideoCardUrlList } from "../../constants/exam/JeeAdvanceExamPageConstants"
+import { BitSatContentList, BitSatExamPageButtonCodesList, BitSatExamPageButtonWidthList, BitSatExamRoutesList, BitSatExamsButtonNameList, BitSatExamSubMenuList, BitSatHeadingIdList, BitSatHeadingList, BitSatTableOfContentList, BitSatVideoCardTitleList, BitSatVideoCardUrlList } from "../../constants/exam/BitSatExamPageConstants"
 
 
 // this component for the jee advance exam page 
@@ -40,18 +40,18 @@ const BitSatExamPage = () => {
     return (
         <div className="text-white  mt-10 w-full">
             <div className="flex items-center justify-center">
-                <OverviewSectionComponent dropDownMenuName="Overview" dropDownButtonId="JeeAdvanceOverviewButtonId" dropDowndivId="JeeAdvanceOverviewDivId" dropDownSubMenuList={JeeAdvanceExamSubMenuList} dropDownRoutesList={JeeAdvanceExamRoutesList} buttonNameList={JeeAdvanceExamsButtonNameList} buttonWidthList={JeeAdvanceExamPageButtonWidthList} buttonCodeList={JeeAdvanceExamPageButtonCodesList} buttonGenericClickHandler={OnOverviewSectionButtonClickHandlerGeneric}></OverviewSectionComponent>
+                <OverviewSectionComponent dropDownMenuName="Overview" dropDownButtonId="BitSatOverviewButtonId" dropDowndivId="BitSatOverviewDivId" dropDownSubMenuList={BitSatExamSubMenuList} dropDownRoutesList={BitSatExamRoutesList} buttonNameList={BitSatExamsButtonNameList} buttonWidthList={BitSatExamPageButtonWidthList} buttonCodeList={BitSatExamPageButtonCodesList} buttonGenericClickHandler={OnOverviewSectionButtonClickHandlerGeneric}></OverviewSectionComponent>
             </div>
-            <HeadingComponent headingName="Latest Updates" headingSpanName="Jee Advance"></HeadingComponent>
+            <HeadingComponent headingName="Latest Updates" headingSpanName="BITSAT"></HeadingComponent>
             <div className="flex justify-center items-center justify-evenly mt-10 w-full p-10">
                 {/* here we will be using the map function to render all the available video card  component */}
-                {JeeAdvanceVideoCardTitleList.map((_ : string, index : number) => (
-                    <VideoCard key={index} videoUrl={JeeAdvanceVideoCardUrlList[index]} title={JeeAdvanceVideoCardTitleList[index]} content="An example of a YouTube video embedded in a Tailwind CSS card."></VideoCard>
+                {BitSatVideoCardUrlList.map((_ : string, index : number) => (
+                    <VideoCard key={index} videoUrl={BitSatVideoCardUrlList[index]} title={BitSatVideoCardTitleList[index]} content="An example of a YouTube video embedded in a Tailwind CSS card."></VideoCard>
                 ))}
             </div>
             <div className="">
                 {/* <BlogHeadingComponent headingName="JEE MAINS EXAM"></BlogHeadingComponent> */}
-                <BlogComponent headingIdList={JeeAdvanceHeadingIdList} headingNameList={JeeAdvanceHeadingList} tableOfContentsList={JeeAdvanceTableOfContentList} contentList={JeeAdvanceContentList}></BlogComponent>
+                <BlogComponent headingIdList={BitSatHeadingIdList} headingNameList={BitSatHeadingList} tableOfContentsList={BitSatTableOfContentList} contentList={BitSatContentList}></BlogComponent>
 
             </div>
         </div>
