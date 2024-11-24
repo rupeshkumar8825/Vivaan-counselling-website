f71c973ac6c6ba129cc7aaf2602aa3be2a73add5
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-11-24 12:20:05 +0530

    Feature : Add feature for wbjee counselling page

---
+++ src/constants/councelling/WbJeeCounsellingPageConstants.tsx
@@ -0,0 +1,150 @@
+// constant page for for WbJee counselling page
+// constants for the jacdelhi counselling page for this purpose
+// constant page for the csab related stuff 
+// here we will store the constants related to the josaa counselling page 
+
+
+// OVERVIEW  COMPONENT FOR JOSAA COUNSELLING PAGE 
+// constant for storing the submenu names which we have to show in dropdown
+export const WbJEECounsellingPageSubMenuList : Array<string> = [
+    "What is WbJEE Counselling?",
+    "WbJEE Counselling Documents",
+    "WbJEE Counselling Application Process",
+    "WbJEE Counselling Fee",
+    "WbJEE Counselling Cutoff",
+    "WbJEE Counselling Online Reporting",
+    "WbJEE Counselling Physical Reporting",
+]
+
+
+
+// constant for the submenu list routes that we need inside the overview dropdown button
+export const WbJEECounsellingPageRoutesList : Array<string> = [
+    "/counselling/wbjee/introduction", 
+    "/counselling/wbjee/documents", 
+    "/counselling/wbjee/application-process", 
+    "/counselling/wbjee/fee", 
+    "/counselling/wbjee/cutoff", 
+    "/counselling/wbjee/online-reporting",
+    "/counselling/wbjee/physical-reporting",
+]
+
+
+export const WbJEECounsellingPageButtonNameList : Array<string> = [
+    "JOSAA",
+    "CSAB",
+    "JAC DELHI",
+    "WBJEE",
+    "UPTU", 
+    "GGSIPU",
+    "COMEDK",
+]
+
+export const WbJEECounsellingPageButtonWidthList : Array<string> = [
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+]
+
+
+export const WbJEECounsellingPageButtonCodeList : Array<string> = [
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5",
+    "6",
+    "7"
+]
+
+
+
+// Video card related constants for the Csab counselling page
+export const WbJEECounsellingPageVideoCardUrlList : Array<string> = [
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I"
+]
+
+export const WbJEECounsellingPageVideoCardTitleList : Array<string> = [
+    "First title", 
+    "Second title", 
+    "Third title"
+]
+
+
+// BLOGS Related constants for Csab Counselling
+// this consists of the constants related to the heading name, headingID 
+// content that we have to put in each of the headings
+export const WbJEECounsellingPageHeadingList : Array<string> = [
+    "What is WbJEE Counselling?", 
+    "WbJEE Counselling dates 2025", 
+    "WbJEE Counselling Eligibility Criteria", 
+    "WbJEE Counselling process 2025",
+    "WbJEE Counselling counselling application form",
+    "Document required for WbJEE Counselling",
+    "WbJEE Counselling Choice filling 2025",
+    "WbJEE Counselling Seat Allotment 2025", 
+    "WbJEE Counselling 2025 Opening and Closing rank",
+    "WbJEE Counselling fee 2025",
+    "WbJEE Counselling Seat Allotment Letter",
+    "WbJEE Counselling Online Reporting", 
+    "WbJEE Counselling Physical Reporting allotted", 
+    "WbJEE Counselling FAQ"
+]
+
+export const WbJEECounsellingPageTableOfContentList : Array<string> = [
+    "What is WbJEE Counselling?", 
+    "WbJEE Counselling dates 2025", 
+    "WbJEE Counselling Eligibility Criteria", 
+    "WbJEE Counselling process 2025",
+    "WbJEE Counselling counselling application form",
+    "Document required for WbJEE Counselling",
+    "WbJEE Counselling Choice filling 2025",
+    "WbJEE Counselling Seat Allotment 2025", 
+    "WbJEE Counselling 2025 Opening and Closing rank",
+    "WbJEE Counselling fee 2025",
+    "WbJEE Counselling Seat Allotment Letter",
+    "WbJEE Counselling Online Reporting", 
+    "WbJEE Counselling Physical Reporting allotted", 
+    "WbJEE Counselling FAQ"
+]
+
+export const WbJEECounsellingPageHeadingIdList : Array<string> = [
+    "counselling-wbjee-h1",
+    "counselling-wbjee-h2",
+    "counselling-wbjee-h3",
+    "counselling-wbjee-h4",
+    "counselling-wbjee-h5",
+    "counselling-wbjee-h6",
+    "counselling-wbjee-h7",
+    "counselling-wbjee-h8",
+    "counselling-wbjee-h9",
+    "counselling-wbjee-h10",
+    "counselling-wbjee-h11",
+    "counselling-wbjee-h12",
+    "counselling-wbjee-h13",
+    "counselling-wbjee-h14",
+]
+
+export const WbJEECounsellingPageContentList : Array<string> = [
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ipsam eos ratione. Labore dolorum accusantium suscipit? Autem numquam ducimus dolores doloremque illum, possimus porro quisquam, suscipit, nobis voluptas laboriosam dicta Quibusdam ad aperiam veritatis, aut nemo magnam incidunt, adipisci quaerat optio, necessitatibus ullam esse enim suscipit veniam debitis. Placeat accusamus ratione incidunt cupiditate iure. Vitae recusandae culpa temporibus quae minima? Error provident aliquid soluta, expedita perferendis eaque quasi aperiam debitis quam unde, sequi placeat cumque quisquam quia consequuntur dolorum, ipsam rerum nostrum iure asperiores deleniti eos. Modi perferendis recusandae asperiores?", 
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    
+]
--- src/pages/counselling/WebJeeCounsellingPage.tsx
+++ src/pages/counselling/WebJeeCounsellingPage.tsx
@@ -1,9 +1,84 @@
-const WebJeeCounsellingPage = () => {
+import HeadingComponent from "../../components/HeadingComponent"
+import OverviewSectionComponent from "../../home/components/OverviewSectionComponent"
+import VideoCard from "../../components/VideoCardComponent"
+import BlogComponent from "../../components/blog/BlogComponent"
+import { useNavigate } from "react-router-dom"
+import { 
+    WbJEECounsellingPageButtonCodeList, 
+    WbJEECounsellingPageButtonNameList, 
+    WbJEECounsellingPageButtonWidthList, 
+    WbJEECounsellingPageContentList, 
+    WbJEECounsellingPageHeadingIdList, 
+    WbJEECounsellingPageHeadingList, 
+    WbJEECounsellingPageRoutesList, 
+    WbJEECounsellingPageSubMenuList, 
+    WbJEECounsellingPageTableOfContentList, 
+    WbJEECounsellingPageVideoCardTitleList, 
+    WbJEECounsellingPageVideoCardUrlList 
+} from "../../constants/councelling/WbJeeCounsellingPageConstants"
+
+// component for showing the wbjee counselling form for this purpose
+const WbJEECounsellingPage = () => {
+    const navigate = useNavigate()
+
+    // this function is generic click hander for the jee main exam page's overview section
+    // same function will be implemented for the other exam pages like the jee advance, bitsat, wbjee and so on
+    // hence this function is made generic to handle the click handlers present in a particular exam page
+    const OnOverviewSectionButtonClickHandlerGeneric = (buttonCode : string) => {
+        // we have to know on which button the user has clicked 
+        // and then we will be to decide where to redirect the user
+        // can we pass some of the identifiers to identify this
+        // let us have a code generic code to distinguish between the buttons 
+        // using the if else statement for this purpose
+        if (buttonCode === "1") 
+        {
+            navigate("/counselling/josaa")
+        }
+        else if(buttonCode === "2")
+        {
+            navigate("/counselling/csab")
+        }
+        else if (buttonCode === "3")
+        {
+            navigate("/counselling/jacdelhi")
+        }
+        else if (buttonCode === "4")
+        {
+            navigate("/counselling/wbjee")
+        }
+        else if (buttonCode === "5")
+        {
+            navigate("/counselling/uptu")
+        }
+        else if (buttonCode === "6")
+        {
+            navigate("/counselling/ggsipu")
+        }
+        else if (buttonCode === "7")
+        {
+            navigate("/counselling/comedk")
+        }
+    }
     return (
-        <div>
-            Welcome to WebJEE councelling page. 
+        <div className="text-white  mt-10 w-full">
+            <div className="flex items-center justify-center">
+                <OverviewSectionComponent dropDownMenuName="Overview" dropDownButtonId="WbJEEOverviewButtonId" dropDowndivId="WbJEEOverviewDivId" dropDownSubMenuList={WbJEECounsellingPageSubMenuList} dropDownRoutesList={WbJEECounsellingPageRoutesList} buttonNameList={WbJEECounsellingPageButtonNameList} buttonWidthList={WbJEECounsellingPageButtonWidthList} buttonCodeList={WbJEECounsellingPageButtonCodeList} buttonGenericClickHandler={OnOverviewSectionButtonClickHandlerGeneric}></OverviewSectionComponent>
+            </div>
+            <HeadingComponent headingName="Latest Updates" headingSpanName="WbJEE Counselling"></HeadingComponent>
+            <div className="flex justify-center items-center justify-evenly mt-10 w-full p-10">
+                {/* here we will be using the map function to render all the available video card  component */}
+                {WbJEECounsellingPageVideoCardUrlList.map((_ : string, index : number) => (
+                    <VideoCard key={index} videoUrl={WbJEECounsellingPageVideoCardUrlList[index]} title={WbJEECounsellingPageVideoCardTitleList[index]} content="An example of a YouTube video embedded in a Tailwind CSS card."></VideoCard>
+                ))}
+            </div>
+            <div className="">
+                {/* <BlogHeadingComponent headingName="JEE MAINS EXAM"></BlogHeadingComponent> */}
+                <BlogComponent headingIdList={WbJEECounsellingPageHeadingIdList} headingNameList={WbJEECounsellingPageHeadingList} tableOfContentsList={WbJEECounsellingPageTableOfContentList} contentList={WbJEECounsellingPageContentList}></BlogComponent>
+
+            </div>
         </div>
     )
 }
 
-export default WebJeeCounsellingPage
+// say everything went fine 
+export default WbJEECounsellingPage
