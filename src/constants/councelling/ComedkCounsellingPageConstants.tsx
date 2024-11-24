23c889d9cc76505412a418e5d1191b89cb8f4636
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-11-24 12:48:57 +0530

    Feature : Add page for comedk counselling home page

---
+++ src/constants/councelling/ComedkCounsellingPageConstants.tsx
@@ -0,0 +1,152 @@
+// this is the constant page for the Comedk counselling page
+// constants page for uptu counselling home page
+// constant page for for WbJee counselling page
+// constants for the jacdelhi counselling page for this purpose
+// constant page for the csab related stuff 
+// here we will store the constants related to the josaa counselling page 
+
+
+// OVERVIEW  COMPONENT FOR JOSAA COUNSELLING PAGE 
+// constant for storing the submenu names which we have to show in dropdown
+export const ComedkCounsellingPageSubMenuList : Array<string> = [
+    "What is Comedk Counselling?",
+    "Comedk Counselling Documents",
+    "Comedk Counselling Application Process",
+    "Comedk Counselling Fee",
+    "Comedk Counselling Cutoff",
+    "Comedk Counselling Online Reporting",
+    "Comedk Counselling Physical Reporting",
+]
+
+
+
+// constant for the submenu list routes that we need inside the overview dropdown button
+export const ComedkCounsellingPageRoutesList : Array<string> = [
+    "/counselling/comedk/introduction", 
+    "/counselling/comedk/documents", 
+    "/counselling/comedk/application-process", 
+    "/counselling/comedk/fee", 
+    "/counselling/comedk/cutoff", 
+    "/counselling/comedk/online-reporting",
+    "/counselling/comedk/physical-reporting",
+]
+
+
+export const ComedkCounsellingPageButtonNameList : Array<string> = [
+    "JOSAA",
+    "CSAB",
+    "JAC DELHI",
+    "WBJEE",
+    "UPTU", 
+    "GGSIPU",
+    "COMEDK",
+]
+
+export const ComedkCounsellingPageButtonWidthList : Array<string> = [
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+]
+
+
+export const ComedkCounsellingPageButtonCodeList : Array<string> = [
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5",
+    "6",
+    "7"
+]
+
+
+
+// Video card related constants for the Csab counselling page
+export const ComedkCounsellingPageVideoCardUrlList : Array<string> = [
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I"
+]
+
+export const ComedkCounsellingPageVideoCardTitleList : Array<string> = [
+    "First title", 
+    "Second title", 
+    "Third title"
+]
+
+
+// BLOGS Related constants for Csab Counselling
+// this consists of the constants related to the heading name, headingID 
+// content that we have to put in each of the headings
+export const ComedkCounsellingPageHeadingList : Array<string> = [
+    "What is Comedk Counselling?", 
+    "Comedk Counselling dates 2025", 
+    "Comedk Counselling Eligibility Criteria", 
+    "Comedk Counselling process 2025",
+    "Comedk Counselling counselling application form",
+    "Document required for Comedk Counselling",
+    "Comedk Counselling Choice filling 2025",
+    "Comedk Counselling Seat Allotment 2025", 
+    "Comedk Counselling 2025 Opening and Closing rank",
+    "Comedk Counselling fee 2025",
+    "Comedk Counselling Seat Allotment Letter",
+    "Comedk Counselling Online Reporting", 
+    "Comedk Counselling Physical Reporting allotted", 
+    "Comedk Counselling FAQ"
+]
+
+export const ComedkCounsellingPageTableOfContentList : Array<string> = [
+    "What is Comedk Counselling?", 
+    "Comedk Counselling dates 2025", 
+    "Comedk Counselling Eligibility Criteria", 
+    "Comedk Counselling process 2025",
+    "Comedk Counselling counselling application form",
+    "Document required for Comedk Counselling",
+    "Comedk Counselling Choice filling 2025",
+    "Comedk Counselling Seat Allotment 2025", 
+    "Comedk Counselling 2025 Opening and Closing rank",
+    "Comedk Counselling fee 2025",
+    "Comedk Counselling Seat Allotment Letter",
+    "Comedk Counselling Online Reporting", 
+    "Comedk Counselling Physical Reporting allotted", 
+    "Comedk Counselling FAQ"
+]
+
+export const ComedkCounsellingPageHeadingIdList : Array<string> = [
+    "counselling-uptu-h1",
+    "counselling-uptu-h2",
+    "counselling-uptu-h3",
+    "counselling-uptu-h4",
+    "counselling-uptu-h5",
+    "counselling-uptu-h6",
+    "counselling-uptu-h7",
+    "counselling-uptu-h8",
+    "counselling-uptu-h9",
+    "counselling-uptu-h10",
+    "counselling-uptu-h11",
+    "counselling-uptu-h12",
+    "counselling-uptu-h13",
+    "counselling-uptu-h14",
+]
+
+export const ComedkCounsellingPageContentList : Array<string> = [
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ipsam eos ratione. Labore dolorum accusantium suscipit? Autem numquam ducimus dolores doloremque illum, possimus porro quisquam, suscipit, nobis voluptas laboriosam dicta Quibusdam ad aperiam veritatis, aut nemo magnam incidunt, adipisci quaerat optio, necessitatibus ullam esse enim suscipit veniam debitis. Placeat accusamus ratione incidunt cupiditate iure. Vitae recusandae culpa temporibus quae minima? Error provident aliquid soluta, expedita perferendis eaque quasi aperiam debitis quam unde, sequi placeat cumque quisquam quia consequuntur dolorum, ipsam rerum nostrum iure asperiores deleniti eos. Modi perferendis recusandae asperiores?", 
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    
+]
