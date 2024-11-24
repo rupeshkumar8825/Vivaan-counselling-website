b434928f9428271b6dbc646e6b049f09ed77e402
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-11-24 18:16:29 +0530

    Feature : GGSIPU Counselling home page done

---
+++ src/constants/councelling/GGSIPUCounsellingPageConstants.tsx
@@ -0,0 +1,150 @@
+// this is constants page for GGSIPU counselling page
+// constants for the jacdelhi counselling page for this purpose
+// constant page for the csab related stuff 
+// here we will store the constants related to the josaa counselling page 
+
+
+// OVERVIEW  COMPONENT FOR JOSAA COUNSELLING PAGE 
+// constant for storing the submenu names which we have to show in dropdown
+export const GGSIPUCounsellingPageSubMenuList : Array<string> = [
+    "What is GGSIPU Counselling?",
+    "GGSIPU Counselling Documents",
+    "GGSIPU Counselling Application Process",
+    "GGSIPU Counselling Fee",
+    "GGSIPU Counselling Cutoff",
+    "GGSIPU Counselling Online Reporting",
+    "GGSIPU Counselling Physical Reporting",
+]
+
+
+
+// constant for the submenu list routes that we need inside the overview dropdown button
+export const GGSIPUCounsellingPageRoutesList : Array<string> = [
+    "/counselling/ggsipu/introduction", 
+    "/counselling/ggsipu/documents", 
+    "/counselling/ggsipu/application-process", 
+    "/counselling/ggsipu/fee", 
+    "/counselling/ggsipu/cutoff", 
+    "/counselling/ggsipu/online-reporting",
+    "/counselling/ggsipu/physical-reporting",
+]
+
+
+export const GGSIPUCounsellingPageButtonNameList : Array<string> = [
+    "JOSAA",
+    "CSAB",
+    "JAC DELHI",
+    "WBJEE",
+    "UPTU", 
+    "GGSIPU",
+    "COMEDK",
+]
+
+export const GGSIPUCounsellingPageButtonWidthList : Array<string> = [
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+]
+
+
+export const GGSIPUCounsellingPageButtonCodeList : Array<string> = [
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5",
+    "6",
+    "7"
+]
+
+
+
+// Video card related constants for the Csab counselling page
+export const GGSIPUCounsellingPageVideoCardUrlList : Array<string> = [
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I"
+]
+
+export const GGSIPUCounsellingPageVideoCardTitleList : Array<string> = [
+    "First title", 
+    "Second title", 
+    "Third title"
+]
+
+
+// BLOGS Related constants for Csab Counselling
+// this consists of the constants related to the heading name, headingID 
+// content that we have to put in each of the headings
+export const GGSIPUCounsellingPageHeadingList : Array<string> = [
+    "What is GGSIPU Counselling?", 
+    "GGSIPU Counselling dates 2025", 
+    "GGSIPU Counselling Eligibility Criteria", 
+    "GGSIPU Counselling process 2025",
+    "GGSIPU Counselling counselling application form",
+    "Document required for GGSIPU Counselling",
+    "GGSIPU Counselling Choice filling 2025",
+    "GGSIPU Counselling Seat Allotment 2025", 
+    "GGSIPU Counselling 2025 Opening and Closing rank",
+    "GGSIPU Counselling fee 2025",
+    "GGSIPU Counselling Seat Allotment Letter",
+    "GGSIPU Counselling Online Reporting", 
+    "GGSIPU Counselling Physical Reporting allotted", 
+    "GGSIPU Counselling FAQ"
+]
+
+export const GGSIPUCounsellingPageTableOfContentList : Array<string> = [
+    "What is GGSIPU Counselling?", 
+    "GGSIPU Counselling dates 2025", 
+    "GGSIPU Counselling Eligibility Criteria", 
+    "GGSIPU Counselling process 2025",
+    "GGSIPU Counselling counselling application form",
+    "Document required for GGSIPU Counselling",
+    "GGSIPU Counselling Choice filling 2025",
+    "GGSIPU Counselling Seat Allotment 2025", 
+    "GGSIPU Counselling 2025 Opening and Closing rank",
+    "GGSIPU Counselling fee 2025",
+    "GGSIPU Counselling Seat Allotment Letter",
+    "GGSIPU Counselling Online Reporting", 
+    "GGSIPU Counselling Physical Reporting allotted", 
+    "GGSIPU Counselling FAQ"
+]
+
+export const GGSIPUCounsellingPageHeadingIdList : Array<string> = [
+    "counselling-gssipu-h1",
+    "counselling-ggsipu-h2",
+    "counselling-ggsipu-h3",
+    "counselling-ggsipu-h4",
+    "counselling-ggsipu-h5",
+    "counselling-ggsipu-h6",
+    "counselling-ggsipu-h7",
+    "counselling-ggsipu-h8",
+    "counselling-ggsipu-h9",
+    "counselling-ggsipu-h10",
+    "counselling-ggsipu-h11",
+    "counselling-ggsipu-h12",
+    "counselling-ggsipu-h13",
+    "counselling-ggsipu-h14",
+]
+
+export const GGSIPUCounsellingPageContentList : Array<string> = [
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ipsam eos ratione. Labore dolorum accusantium suscipit? Autem numquam ducimus dolores doloremque illum, possimus porro quisquam, suscipit, nobis voluptas laboriosam dicta Quibusdam ad aperiam veritatis, aut nemo magnam incidunt, adipisci quaerat optio, necessitatibus ullam esse enim suscipit veniam debitis. Placeat accusamus ratione incidunt cupiditate iure. Vitae recusandae culpa temporibus quae minima? Error provident aliquid soluta, expedita perferendis eaque quasi aperiam debitis quam unde, sequi placeat cumque quisquam quia consequuntur dolorum, ipsam rerum nostrum iure asperiores deleniti eos. Modi perferendis recusandae asperiores?", 
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    
+]
--- src/pages/counselling/GgsipuCounsellingPage.tsx
+++ src/pages/counselling/GgsipuCounsellingPage.tsx
@@ -1,9 +1,74 @@
-const GgsipuCounsellingPage = () => {
+import HeadingComponent from "../../components/HeadingComponent"
+import OverviewSectionComponent from "../../home/components/OverviewSectionComponent"
+import VideoCard from "../../components/VideoCardComponent"
+import BlogComponent from "../../components/blog/BlogComponent"
+import { useNavigate } from "react-router-dom"
+import { GGSIPUCounsellingPageButtonCodeList, GGSIPUCounsellingPageButtonNameList, GGSIPUCounsellingPageButtonWidthList, GGSIPUCounsellingPageContentList, GGSIPUCounsellingPageHeadingIdList, GGSIPUCounsellingPageHeadingList, GGSIPUCounsellingPageRoutesList, GGSIPUCounsellingPageSubMenuList, GGSIPUCounsellingPageTableOfContentList, GGSIPUCounsellingPageVideoCardTitleList, GGSIPUCounsellingPageVideoCardUrlList } from "../../constants/councelling/GGSIPUCounsellingPageConstants"
+
+
+// component for the GGSIPU councelling page for this purpose
+const GGSIPUCounsellingPage = () => {
+    const navigate = useNavigate()
+
+    // this function is generic click hander for the jee main exam page's overview section
+    // same function will be implemented for the other exam pages like the jee advance, bitsat, wbjee and so on
+    // hence this function is made generic to handle the click handlers present in a particular exam page
+    const OnOverviewSectionButtonClickHandlerGeneric = (buttonCode : string) => {
+        // we have to know on which button the user has clicked 
+        // and then we will be to decide where to redirect the user
+        // can we pass some of the identifiers to identify this
+        // let us have a code generic code to distinguish between the buttons 
+        // using the if else statement for this purpose
+        if (buttonCode === "1") 
+        {
+            navigate("/counselling/josaa")
+        }
+        else if(buttonCode === "2")
+        {
+            navigate("/counselling/csab")
+        }
+        else if (buttonCode === "3")
+        {
+            navigate("/counselling/jacdelhi")
+        }
+        else if (buttonCode === "4")
+        {
+            navigate("/counselling/wbjee")
+        }
+        else if (buttonCode === "5")
+        {
+            navigate("/counselling/uptu")
+        }
+        else if (buttonCode === "6")
+        {
+            navigate("/counselling/ggsipu")
+        }
+        else if (buttonCode === "7")
+        {
+            navigate("/counselling/comedk")
+        }
+    }
+
     return (
-        <div>
-            Welcome to GGSIPU Counselling page. 
+        <div className="text-white  mt-10 w-full">
+            <div className="flex items-center justify-center">
+                <OverviewSectionComponent dropDownMenuName="Overview" dropDownButtonId="GGSIPUOverviewButtonId" dropDowndivId="GGSIPUOverviewDivId" dropDownSubMenuList={GGSIPUCounsellingPageSubMenuList} dropDownRoutesList={GGSIPUCounsellingPageRoutesList} buttonNameList={GGSIPUCounsellingPageButtonNameList} buttonWidthList={GGSIPUCounsellingPageButtonWidthList} buttonCodeList={GGSIPUCounsellingPageButtonCodeList} buttonGenericClickHandler={OnOverviewSectionButtonClickHandlerGeneric}></OverviewSectionComponent>
+            </div>
+            <HeadingComponent headingName="Latest Updates" headingSpanName="GGSIPU Counselling"></HeadingComponent>
+            <div className="flex justify-center items-center justify-evenly mt-10 w-full p-10">
+                {/* here we will be using the map function to render all the available video card  component */}
+                {GGSIPUCounsellingPageVideoCardUrlList.map((_ : string, index : number) => (
+                    <VideoCard key={index} videoUrl={GGSIPUCounsellingPageVideoCardUrlList[index]} title={GGSIPUCounsellingPageVideoCardTitleList[index]} content="An example of a YouTube video embedded in a Tailwind CSS card."></VideoCard>
+                ))}
+            </div>
+            <div className="">
+                {/* <BlogHeadingComponent headingName="JEE MAINS EXAM"></BlogHeadingComponent> */}
+                <BlogComponent headingIdList={GGSIPUCounsellingPageHeadingIdList} headingNameList={GGSIPUCounsellingPageHeadingList} tableOfContentsList={GGSIPUCounsellingPageTableOfContentList} contentList={GGSIPUCounsellingPageContentList}></BlogComponent>
+
+            </div>
         </div>
     )
 }
 
-export default GgsipuCounsellingPage
+// say everything went fine 
+export default GGSIPUCounsellingPage
