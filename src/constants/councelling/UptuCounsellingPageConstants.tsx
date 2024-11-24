ad1c4b6c4a7e1132b044b2bc80b05694045e254e
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-11-24 12:34:40 +0530

    Feature : Add UPTU Counselling home page for this purpose

---
+++ src/constants/councelling/UptuCounsellingPageConstants.tsx
@@ -0,0 +1,151 @@
+// constants page for uptu counselling home page
+// constant page for for WbJee counselling page
+// constants for the jacdelhi counselling page for this purpose
+// constant page for the csab related stuff 
+// here we will store the constants related to the josaa counselling page 
+
+
+// OVERVIEW  COMPONENT FOR JOSAA COUNSELLING PAGE 
+// constant for storing the submenu names which we have to show in dropdown
+export const UptuCounsellingPageSubMenuList : Array<string> = [
+    "What is Uptu Counselling?",
+    "Uptu Counselling Documents",
+    "Uptu Counselling Application Process",
+    "Uptu Counselling Fee",
+    "Uptu Counselling Cutoff",
+    "Uptu Counselling Online Reporting",
+    "Uptu Counselling Physical Reporting",
+]
+
+
+
+// constant for the submenu list routes that we need inside the overview dropdown button
+export const UptuCounsellingPageRoutesList : Array<string> = [
+    "/counselling/uptu/introduction", 
+    "/counselling/uptu/documents", 
+    "/counselling/uptu/application-process", 
+    "/counselling/uptu/fee", 
+    "/counselling/uptu/cutoff", 
+    "/counselling/uptu/online-reporting",
+    "/counselling/uptu/physical-reporting",
+]
+
+
+export const UptuCounsellingPageButtonNameList : Array<string> = [
+    "JOSAA",
+    "CSAB",
+    "JAC DELHI",
+    "WBJEE",
+    "UPTU", 
+    "GGSIPU",
+    "COMEDK",
+]
+
+export const UptuCounsellingPageButtonWidthList : Array<string> = [
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+    "36",
+]
+
+
+export const UptuCounsellingPageButtonCodeList : Array<string> = [
+    "1", 
+    "2", 
+    "3", 
+    "4", 
+    "5",
+    "6",
+    "7"
+]
+
+
+
+// Video card related constants for the Csab counselling page
+export const UptuCounsellingPageVideoCardUrlList : Array<string> = [
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I", 
+    "https://www.youtube.com/embed/CHmaw6Vu4_I"
+]
+
+export const UptuCounsellingPageVideoCardTitleList : Array<string> = [
+    "First title", 
+    "Second title", 
+    "Third title"
+]
+
+
+// BLOGS Related constants for Csab Counselling
+// this consists of the constants related to the heading name, headingID 
+// content that we have to put in each of the headings
+export const UptuCounsellingPageHeadingList : Array<string> = [
+    "What is Uptu Counselling?", 
+    "Uptu Counselling dates 2025", 
+    "Uptu Counselling Eligibility Criteria", 
+    "Uptu Counselling process 2025",
+    "Uptu Counselling counselling application form",
+    "Document required for Uptu Counselling",
+    "Uptu Counselling Choice filling 2025",
+    "Uptu Counselling Seat Allotment 2025", 
+    "Uptu Counselling 2025 Opening and Closing rank",
+    "Uptu Counselling fee 2025",
+    "Uptu Counselling Seat Allotment Letter",
+    "Uptu Counselling Online Reporting", 
+    "Uptu Counselling Physical Reporting allotted", 
+    "Uptu Counselling FAQ"
+]
+
+export const UptuCounsellingPageTableOfContentList : Array<string> = [
+    "What is Uptu Counselling?", 
+    "Uptu Counselling dates 2025", 
+    "Uptu Counselling Eligibility Criteria", 
+    "Uptu Counselling process 2025",
+    "Uptu Counselling counselling application form",
+    "Document required for Uptu Counselling",
+    "Uptu Counselling Choice filling 2025",
+    "Uptu Counselling Seat Allotment 2025", 
+    "Uptu Counselling 2025 Opening and Closing rank",
+    "Uptu Counselling fee 2025",
+    "Uptu Counselling Seat Allotment Letter",
+    "Uptu Counselling Online Reporting", 
+    "Uptu Counselling Physical Reporting allotted", 
+    "Uptu Counselling FAQ"
+]
+
+export const UptuCounsellingPageHeadingIdList : Array<string> = [
+    "counselling-uptu-h1",
+    "counselling-uptu-h2",
+    "counselling-uptu-h3",
+    "counselling-uptu-h4",
+    "counselling-uptu-h5",
+    "counselling-uptu-h6",
+    "counselling-uptu-h7",
+    "counselling-uptu-h8",
+    "counselling-uptu-h9",
+    "counselling-uptu-h10",
+    "counselling-uptu-h11",
+    "counselling-uptu-h12",
+    "counselling-uptu-h13",
+    "counselling-uptu-h14",
+]
+
+export const UptuCounsellingPageContentList : Array<string> = [
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ipsam eos ratione. Labore dolorum accusantium suscipit? Autem numquam ducimus dolores doloremque illum, possimus porro quisquam, suscipit, nobis voluptas laboriosam dicta Quibusdam ad aperiam veritatis, aut nemo magnam incidunt, adipisci quaerat optio, necessitatibus ullam esse enim suscipit veniam debitis. Placeat accusamus ratione incidunt cupiditate iure. Vitae recusandae culpa temporibus quae minima? Error provident aliquid soluta, expedita perferendis eaque quasi aperiam debitis quam unde, sequi placeat cumque quisquam quia consequuntur dolorum, ipsam rerum nostrum iure asperiores deleniti eos. Modi perferendis recusandae asperiores?", 
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque esse debitis labore, sapiente eum nobis, quasi atque modi sequi facere corporis, iure optio officia praesentium veritatis minus mollitia officiis.Officia sequi, dolorum voluptatum consectetur beatae dolore iure adipisci exercitationem aspernatur accusantium aperiam ipsam tempore molestias odit quam libero ad? Fugiat, expedita cupiditate. Aut corrupti animi error architecto maxime iure?",
+    
+]
--- src/pages/counselling/UptuCounsellingPage.tsx
+++ src/pages/counselling/UptuCounsellingPage.tsx
@@ -1,9 +1,84 @@
+import HeadingComponent from "../../components/HeadingComponent"
+import OverviewSectionComponent from "../../home/components/OverviewSectionComponent"
+import VideoCard from "../../components/VideoCardComponent"
+import BlogComponent from "../../components/blog/BlogComponent"
+import { useNavigate } from "react-router-dom"
+import { 
+    UptuCounsellingPageButtonCodeList, 
+    UptuCounsellingPageButtonNameList, 
+    UptuCounsellingPageButtonWidthList, 
+    UptuCounsellingPageContentList, 
+    UptuCounsellingPageHeadingIdList, 
+    UptuCounsellingPageHeadingList, 
+    UptuCounsellingPageRoutesList, 
+    UptuCounsellingPageSubMenuList, 
+    UptuCounsellingPageTableOfContentList, 
+    UptuCounsellingPageVideoCardTitleList, 
+    UptuCounsellingPageVideoCardUrlList 
+} from "../../constants/councelling/UptuCounsellingPageConstants"
+
+// component for showing the Uptu counselling form for this purpose
 const UptuCounsellingPage = () => {
+    const navigate = useNavigate()
+
+    // this function is generic click hander for the jee main exam page's overview section
+    // same function will be implemented for the other exam pages like the jee advance, bitsat, Uptu and so on
+    // hence this function is made generic to handle the click handlers present in a particular exam page
+    const OnOverviewSectionButtonClickHandlerGeneric = (buttonCode : string) => {
+        // we have to know on which button the user has clicked 
+        // and then we will be to decide where to redirect the user
+        // can we pass some of the identifiers to identify this
+        // let us have a code generic code to distinguish between the buttons 
+        // using the if else statement for this purpose
+        if (buttonCode === "1") 
+        {
+            navigate("/counselling/josaa")
+        }
+        else if(buttonCode === "2")
+        {
+            navigate("/counselling/csab")
+        }
+        else if (buttonCode === "3")
+        {
+            navigate("/counselling/jacdelhi")
+        }
+        else if (buttonCode === "4")
+        {
+            navigate("/counselling/wbjee")
+        }
+        else if (buttonCode === "5")
+        {
+            navigate("/counselling/uptu")
+        }
+        else if (buttonCode === "6")
+        {
+            navigate("/counselling/ggsipu")
+        }
+        else if (buttonCode === "7")
+        {
+            navigate("/counselling/comedk")
+        }
+    }
     return (
-        <div>
-            Welcome to Uptu councelling page. 
+        <div className="text-white  mt-10 w-full">
+            <div className="flex items-center justify-center">
+                <OverviewSectionComponent dropDownMenuName="Overview" dropDownButtonId="UptuOverviewButtonId" dropDowndivId="UptuOverviewDivId" dropDownSubMenuList={UptuCounsellingPageSubMenuList} dropDownRoutesList={UptuCounsellingPageRoutesList} buttonNameList={UptuCounsellingPageButtonNameList} buttonWidthList={UptuCounsellingPageButtonWidthList} buttonCodeList={UptuCounsellingPageButtonCodeList} buttonGenericClickHandler={OnOverviewSectionButtonClickHandlerGeneric}></OverviewSectionComponent>
+            </div>
+            <HeadingComponent headingName="Latest Updates" headingSpanName="Uptu Counselling"></HeadingComponent>
+            <div className="flex justify-center items-center justify-evenly mt-10 w-full p-10">
+                {/* here we will be using the map function to render all the available video card  component */}
+                {UptuCounsellingPageVideoCardUrlList.map((_ : string, index : number) => (
+                    <VideoCard key={index} videoUrl={UptuCounsellingPageVideoCardUrlList[index]} title={UptuCounsellingPageVideoCardTitleList[index]} content="An example of a YouTube video embedded in a Tailwind CSS card."></VideoCard>
+                ))}
+            </div>
+            <div className="">
+                {/* <BlogHeadingComponent headingName="JEE MAINS EXAM"></BlogHeadingComponent> */}
+                <BlogComponent headingIdList={UptuCounsellingPageHeadingIdList} headingNameList={UptuCounsellingPageHeadingList} tableOfContentsList={UptuCounsellingPageTableOfContentList} contentList={UptuCounsellingPageContentList}></BlogComponent>
+
+            </div>
         </div>
     )
 }
 
+// say everything went fine 
 export default UptuCounsellingPage
