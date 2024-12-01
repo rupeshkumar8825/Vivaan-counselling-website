d3564f3822761272057324c178b851b1855aa572
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-12-01 10:04:30 +0530

    Wip : Add constants for fee section for all the IIITs colleges. This will consists the breakdown and the corresponding fee for this purpose

---
+++ src/constants/college/IIIT/IIITFeesDataCollegeConstant.tsx
@@ -0,0 +1,785 @@
+export const IIITHyderabadFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,200"
+    ],
+    [
+        "One Time Fees",
+        "5,000"
+    ],
+    [
+        "Tuition Fees",
+        "1,00,000"
+    ],
+    [
+        "Other Fees",
+        "4,500"
+    ],
+    [
+        "Annual Fees",
+        "2,390"
+    ]
+]
+
+export const IIITHyderabadFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+
+
+export const IIITGwaliorFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,800"
+    ],
+    [
+        "One Time Fees",
+        "4,500"
+    ],
+    [
+        "Tuition Fees",
+        "90,000"
+    ],
+    [
+        "Other Fees",
+        "4,000"
+    ],
+    [
+        "Annual Fees",
+        "2,200"
+    ]
+]
+
+export const IIITGwaliorFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITBangaloreFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,500"
+    ],
+    [
+        "One Time Fees",
+        "5,200"
+    ],
+    [
+        "Tuition Fees",
+        "1,10,000"
+    ],
+    [
+        "Other Fees",
+        "4,800"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITBangaloreFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITGuwahatiFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,900"
+    ],
+    [
+        "One Time Fees",
+        "4,700"
+    ],
+    [
+        "Tuition Fees",
+        "95,000"
+    ],
+    [
+        "Other Fees",
+        "4,300"
+    ],
+    [
+        "Annual Fees",
+        "2,300"
+    ]
+]
+
+export const IIITGuwahatiFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITJabalpurFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,000"
+    ],
+    [
+        "One Time Fees",
+        "4,800"
+    ],
+    [
+        "Tuition Fees",
+        "1,05,000"
+    ],
+    [
+        "Other Fees",
+        "4,600"
+    ],
+    [
+        "Annual Fees",
+        "2,400"
+    ]
+]
+
+export const IIITJabalpurFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITAllahabadFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,300"
+    ],
+    [
+        "One Time Fees",
+        "5,100"
+    ],
+    [
+        "Tuition Fees",
+        "1,15,000"
+    ],
+    [
+        "Other Fees",
+        "4,900"
+    ],
+    [
+        "Annual Fees",
+        "2,600"
+    ]
+]
+
+export const IIITAllahabadFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITKanchipuramFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,100"
+    ],
+    [
+        "One Time Fees",
+        "5,000"
+    ],
+    [
+        "Tuition Fees",
+        "1,12,000"
+    ],
+    [
+        "Other Fees",
+        "4,700"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITKanchipuramFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITBhubaneshwarFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,900"
+    ],
+    [
+        "One Time Fees",
+        "4,800"
+    ],
+    [
+        "Tuition Fees",
+        "1,00,000"
+    ],
+    [
+        "Other Fees",
+        "4,500"
+    ],
+    [
+        "Annual Fees",
+        "2,400"
+    ]
+]
+
+export const IIITBhubaneshwarFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITPuneFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,200"
+    ],
+    [
+        "One Time Fees",
+        "5,100"
+    ],
+    [
+        "Tuition Fees",
+        "1,10,000"
+    ],
+    [
+        "Other Fees",
+        "4,800"
+    ],
+    [
+        "Annual Fees",
+        "2,600"
+    ]
+]
+
+export const IIITPuneFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITKotaFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,000"
+    ],
+    [
+        "One Time Fees",
+        "4,900"
+    ],
+    [
+        "Tuition Fees",
+        "1,08,000"
+    ],
+    [
+        "Other Fees",
+        "4,600"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITKotaFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITSriCityFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,300"
+    ],
+    [
+        "One Time Fees",
+        "5,200"
+    ],
+    [
+        "Tuition Fees",
+        "1,15,000"
+    ],
+    [
+        "Other Fees",
+        "4,900"
+    ],
+    [
+        "Annual Fees",
+        "2,700"
+    ]
+]
+
+export const IIITSriCityFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITVadodaraFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,100"
+    ],
+    [
+        "One Time Fees",
+        "5,000"
+    ],
+    [
+        "Tuition Fees",
+        "1,12,000"
+    ],
+    [
+        "Other Fees",
+        "4,700"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITVadodaraFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITNagpurFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,200"
+    ],
+    [
+        "One Time Fees",
+        "5,100"
+    ],
+    [
+        "Tuition Fees",
+        "1,10,000"
+    ],
+    [
+        "Other Fees",
+        "4,800"
+    ],
+    [
+        "Annual Fees",
+        "2,600"
+    ]
+]
+
+export const IIITNagpurFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITKalyaniFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,800"
+    ],
+    [
+        "One Time Fees",
+        "4,700"
+    ],
+    [
+        "Tuition Fees",
+        "1,05,000"
+    ],
+    [
+        "Other Fees",
+        "4,500"
+    ],
+    [
+        "Annual Fees",
+        "2,400"
+    ]
+]
+
+export const IIITKalyaniFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITLucknowFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,300"
+    ],
+    [
+        "One Time Fees",
+        "5,200"
+    ],
+    [
+        "Tuition Fees",
+        "1,15,000"
+    ],
+    [
+        "Other Fees",
+        "4,900"
+    ],
+    [
+        "Annual Fees",
+        "2,700"
+    ]
+]
+
+export const IIITLucknowFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITDharwadFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,000"
+    ],
+    [
+        "One Time Fees",
+        "4,900"
+    ],
+    [
+        "Tuition Fees",
+        "1,08,000"
+    ],
+    [
+        "Other Fees",
+        "4,600"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITDharwadFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITBhagalpurFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,900"
+    ],
+    [
+        "One Time Fees",
+        "4,800"
+    ],
+    [
+        "Tuition Fees",
+        "1,00,000"
+    ],
+    [
+        "Other Fees",
+        "4,500"
+    ],
+    [
+        "Annual Fees",
+        "2,400"
+    ]
+]
+
+export const IIITBhagalpurFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITBhopalFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,100"
+    ],
+    [
+        "One Time Fees",
+        "5,000"
+    ],
+    [
+        "Tuition Fees",
+        "1,12,000"
+    ],
+    [
+        "Other Fees",
+        "4,700"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITBhopalFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITKottayamFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,200"
+    ],
+    [
+        "One Time Fees",
+        "5,100"
+    ],
+    [
+        "Tuition Fees",
+        "1,10,000"
+    ],
+    [
+        "Other Fees",
+        "4,800"
+    ],
+    [
+        "Annual Fees",
+        "2,600"
+    ]
+]
+
+export const IIITKottayamFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITRanchiFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,000"
+    ],
+    [
+        "One Time Fees",
+        "4,900"
+    ],
+    [
+        "Tuition Fees",
+        "1,08,000"
+    ],
+    [
+        "Other Fees",
+        "4,600"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITRanchiFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITUnaFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,900"
+    ],
+    [
+        "One Time Fees",
+        "4,800"
+    ],
+    [
+        "Tuition Fees",
+        "1,00,000"
+    ],
+    [
+        "Other Fees",
+        "4,500"
+    ],
+    [
+        "Annual Fees",
+        "2,400"
+    ]
+]
+
+export const IIITUnaFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITSuratFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,100"
+    ],
+    [
+        "One Time Fees",
+        "5,000"
+    ],
+    [
+        "Tuition Fees",
+        "1,12,000"
+    ],
+    [
+        "Other Fees",
+        "4,700"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITSuratFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITManipurFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,300"
+    ],
+    [
+        "One Time Fees",
+        "5,200"
+    ],
+    [
+        "Tuition Fees",
+        "1,15,000"
+    ],
+    [
+        "Other Fees",
+        "4,900"
+    ],
+    [
+        "Annual Fees",
+        "2,700"
+    ]
+]
+
+export const IIITManipurFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITKurnoolFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,000"
+    ],
+    [
+        "One Time Fees",
+        "4,900"
+    ],
+    [
+        "Tuition Fees",
+        "1,08,000"
+    ],
+    [
+        "Other Fees",
+        "4,600"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITKurnoolFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITTiruchirappalliFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,200"
+    ],
+    [
+        "One Time Fees",
+        "5,100"
+    ],
+    [
+        "Tuition Fees",
+        "1,10,000"
+    ],
+    [
+        "Other Fees",
+        "4,800"
+    ],
+    [
+        "Annual Fees",
+        "2,600"
+    ]
+]
+
+export const IIITTiruchirappalliFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITSonepatFeesDataTableConstant = [
+    [
+        "One time Money",
+        "4,800"
+    ],
+    [
+        "One Time Fees",
+        "4,700"
+    ],
+    [
+        "Tuition Fees",
+        "1,05,000"
+    ],
+    [
+        "Other Fees",
+        "4,500"
+    ],
+    [
+        "Annual Fees",
+        "2,400"
+    ]
+]
+
+export const IIITSonepatFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITAgartalaFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,300"
+    ],
+    [
+        "One Time Fees",
+        "5,200"
+    ],
+    [
+        "Tuition Fees",
+        "1,15,000"
+    ],
+    [
+        "Other Fees",
+        "4,900"
+    ],
+    [
+        "Annual Fees",
+        "2,700"
+    ]
+]
+
+export const IIITAgartalaFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
+
+export const IIITKarnatakaFeesDataTableConstant = [
+    [
+        "One time Money",
+        "5,100"
+    ],
+    [
+        "One Time Fees",
+        "5,000"
+    ],
+    [
+        "Tuition Fees",
+        "1,12,000"
+    ],
+    [
+        "Other Fees",
+        "4,700"
+    ],
+    [
+        "Annual Fees",
+        "2,500"
+    ]
+]
+
+export const IIITKarnatakaFeesDataTableHeading = [
+    "Breakdown", 
+    "Amount"
+]
