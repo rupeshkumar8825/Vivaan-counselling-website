3c46d57f1709b7aefc80644c15238c88d644934f
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-12-08 11:04:06 +0530

    Wip : Found out a trick to be able to enter a tab, space and bullet point in string which will render appropriately on the UI

--- src/components/blog/BlogComponent.tsx
+++ src/components/blog/BlogComponent.tsx
@@ -7,7 +7,7 @@ import { IBlogComponent } from '../../interfaces/interfaces';
 // App component for the main layout
 const BlogComponent = (props : IBlogComponent) => {
     return (
-        <div className="container mx-auto p-6">
+        <div className="">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {/* Blog Section - spans 2 columns on medium screens */}
                 <div className="md:col-span-2">
