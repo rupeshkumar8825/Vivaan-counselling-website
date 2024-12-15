896bd7aee90e9e66ac5ada0eaedce4326ba22e2e
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-12-15 11:15:00 +0530

    Feature : Add content for the jee mains page. All the content has added

--- src/constants/exam/JeeMainsExamPageConstants.tsx
+++ src/constants/exam/JeeMainsExamPageConstants.tsx
@@ -1,15 +1,15 @@
 // this file will store the constants related to the jeemain exams page
 import JeeMainExamPatternImage from "../../assets/images/exam/JeeMains/Jee-Mains-Exam-Pattern.png"
 import JeeMainExamDatesImage from "../../assets/images/exam/JeeMains/Jee-Mains-Exam-Dates.png"
-
+import JeeMainQualifyingCriteriaImage from "../../assets/images/exam/JeeMains/Jee-Mains-Qualification-Criteria.png"
 
 export const JeeMainsExamSubMenuList : Array<string> = [
-    "Jee Main Exam",
-    "Jee Main Syllabus",
-    "Jee Main Application",
-    "Jee Main Eligibility",
-    "Jee Main Dates",
-    "Jee Main Admit Card",
+    "JEE Main Exam",
+    "JEE Main Syllabus",
+    "JEE Main Application",
+    "JEE Main Eligibility",
+    "JEE Main Dates",
+    "JEE Main Admit Card",
 ]
 
 
@@ -72,41 +72,41 @@ export const JeeMainVideoCardTitleList : Array<string> = [
 
 // blogs related constants for jee main exam
 export const JeeMainsHeadingList : Array<string> = [
-    "Jee Main Exam History", 
-    "Who Conducts Jee Mains?", 
-    "Jee Main 2025 Exam Dates", 
-    "Jee Main Overview",
-    "Jee Main Exam pattern",
-    "Jee main Cutoff ",
-    "Jee main Eligibility ",
-    "Jee main Qualifications criteria", 
-    "Jee Main Age criteria ",
-    "Jee Main State of eligibility ",
-    "Jee Main Syllabus ",
-    "Jee Main Admit Card", 
+    "JEE Main Exam History", 
+    "Who Conducts JEE Mains?", 
+    "JEE Main 2025 Exam Dates", 
+    "JEE Main Overview",
+    "JEE Main Exam pattern",
+    "JEE main Cutoff ",
+    "JEE main Eligibility ",
+    "JEE main Qualifications criteria", 
+    "JEE Main Age criteria ",
+    "JEE Main State of eligibility ",
+    "JEE Main Syllabus ",
+    "JEE Main Admit Card", 
     "JEE Main Reservation",
     "JEE Main Results",
-    "Jee Main Marks vs Percentile", 
-    "Jee Main Counselling Process"
+    "JEE Main Marks vs Percentile", 
+    "JEE Main Counselling Process"
 ]
 
 export const JeeMainTableOfContentList : Array<string> = [
-    "Jee Main Exam History", 
-    "Who Conducts Jee Mains?", 
-    "Jee Main 2025 Exam Dates", 
-    "Jee Main Overview",
-    "Jee Main Exam pattern",
-    "Jee main Cutoff ",
-    "Jee main Eligibility ",
-    "Jee main Qualifications criteria", 
-    "Jee Main Age criteria ",
-    "Jee Main State of eligibility ",
-    "Jee Main Syllabus ",
-    "Jee Main Admit Card", 
+    "JEE Main Exam History", 
+    "Who Conducts JEE Mains?", 
+    "JEE Main 2025 Exam Dates", 
+    "JEE Main Overview",
+    "JEE Main Exam pattern",
+    "JEE main Cutoff ",
+    "JEE main Eligibility ",
+    "JEE main Qualifications criteria", 
+    "JEE Main Age criteria ",
+    "JEE Main State of eligibility ",
+    "JEE Main Syllabus ",
+    "JEE Main Admit Card", 
     "JEE Main Reservation",
     "JEE Main Results",
-    "Jee Main Marks vs Percentile", 
-    "Jee Main Counselling Process"
+    "JEE Main Marks vs Percentile", 
+    "JEE Main Counselling Process"
 ]
 
 export const JeeMainHeadingIdList : Array<string> = [
@@ -139,7 +139,7 @@ export const JeeMainImageList : Array<string | null> = [
     JeeMainExamPatternImage,
     null,
     null,
-    null,
+    JeeMainQualifyingCriteriaImage,
     null,
     null,
     null,
@@ -155,23 +155,23 @@ export const JeeMainContentList : Array<string> = [
 `• The IIT-JEE (Indian Institutes of Technology Joint Entrance Examination) was first conducted in 1961. It was an entrance examination in India for admission to various undergraduate programs in engineering and technology at the prestigious Indian Institutes of Technology (IITs) and other participating institutions.\n
 • In 2002, an additional exam called the AIEEE was introduced for admissions to many institutions of national importance other than the IITs.\n
 • In 2013, the AIEEE was renamed JEE (Main), and IIT-JEE was renamed JEE (Advanced); the JEE (Main) became the screening exam for JEE (Advanced).\n
-• From 2019 onwards Jee Main is conducted by NTA and Jee Advance is conducted by IIT`,
+• From 2019 onwards JEE Main is conducted by NTA and JEE Advance is conducted by IIT`,
     
     
     
 `The JEE Main exam is administered and conducted by the authority known as the National Testing Agency (NTA). NTA is the agency responsible for conducting the JEE Main every year in two shift first in january and second in april.\n
 
-NTA is conducting jee main exam since 2019 onwards
+NTA is conducting JEE main exam since 2019 onwards
 `,
     
     
     
-`<strong>Jee Main 2025</strong> Exam date for the first attmpt is scheduled form 22nd january to 31st of januray 2025. Jee Main exam date for second session start from`,
+`<strong>JEE Main 2025</strong> Exam date for the first attmpt is scheduled form 22nd january to 31st of januray 2025. JEE Main exam date for second session start from`,
     
     
     
 `<span style = "color : white; font-weight: bold; font-size: larger;" class="underline"> 1. Latest Announcement: </span>\n
-The National Testing Agency (NTA) will announce the JEE Main 2025 exam dates in october 28th 2024. Jee main 2025 exam to be held in two sessions, with Session 1 in January 2025 and Session 2 in April 2025. \n
+The National Testing Agency (NTA) will announce the JEE Main 2025 exam dates in october 28th 2024. JEE main 2025 exam to be held in two sessions, with Session 1 in January 2025 and Session 2 in April 2025. \n
 <span style = "color : white; font-weight: bold; font-size: larger;" class="underline"> 2. Organizer: </span>\n
 Since 2019, National Testing Agency (NTA)\n
 <span style = "color : white; font-weight: bold; font-size: larger;" class="underline"> 3. Purpose : </span>\n
@@ -187,7 +187,7 @@ Since 2019, National Testing Agency (NTA)\n
     
     
     
-`Every year jee main conduct exam in two phase and in each phase there are 75 question each 25 in physics ,chemistry and maths \n
+`Every year JEE main conduct exam in two phase and in each phase there are 75 question each 25 in physics ,chemistry and maths \n
 \t• Paper is divided into two section ,section A and Section B
 \t• Section A contains 20 question which are objective in nature
 \t• Section B contains 5 question which are integer type
@@ -196,51 +196,82 @@ Since 2019, National Testing Agency (NTA)\n
     
     
     
-    `Click here to download the cutoff of JEE Main 2025 <span style = "color : blue;"><a href="https://jeemain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a></span>`,
-    `For jee main exam eligilibilty you must have 12th passed or appearing student A student can give continuously 3 times jee main exam after his/her 12th board.`,
+`To download the cutoff of JEE Main 2025 <span style = "color : white;" class="underline"><a href="https://JEEmain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a></span>`,
     
     
     
-    `A student should either passed the 12th exam or appearing in that particular year in which he/she is going to give exam\n
-    <strong> List of Qualifying Exams: </strong>\n
-    • The final examination of the 10+2 system, conducted by any recognized Central/ State Board, such as the Central Board of Secondary Education, New Delhi; Council for the Indian School Certificate Examinations, New Delhi; etc.\n
-    • Intermediate or two-year Pre-University examination conducted by a recognized Board/University.\n
-    • Final examination of the two-year course of the Joint Services Wing of the National Defense Academy\n
-    • Senior Secondary School Examination conducted by the National Institute of Open Schooling with a minimum of five subjects.\n
-    • Any Public School/ Board/ University examination in India or any foreign country is recognized as equivalent to the 10+2 system by the Association of Indian Universities (AIU).\n
-    • Higher Secondary Certificate Vocational Examination.\n
-    • A Diploma recognized by AICTE or a State board of technical education of at least 3 years duration.\n
-    • General Certificate Education (GCE) examination (London/Cambridge/Sri Lanka) at the Advanced (A) level.\n
-    • High School Certificate Examination of the Cambridge University or International Baccalaureate Diploma of the International Baccalaureate Office, Geneva.\n
-    • Candidates who have completed the Class XII (or equivalent) examination outside India or from a Board not specified above should produce a certificate from the Association of Indian Universities (AIU) to the effect that the examination they have passed is equivalent to the Class XII Examination.\n
-    • In case the Class XII Examination is not a public examination,the candidate must have passed at least one public (Board or Pre-University) examination earlier\n`,
+`For JEE main exam eligilibilty you must have 12th passed or appearing student A student can give continuously 3 times JEE main exam after his/her 12th board.`,
     
     
     
-    `For appearing in the JEE (Main) - 2025, there is no age limit for the candidates. The candidates who have passed the class XII / equivalent examination in 2023, 2024 or appearing in 2025 irrespective of their age can appear in JEE (Main) - 2025 examination. However, the candidates may be required to fulfill the age criteria of the Institute(s) to which they are desirous of taking admission.`,
+`A student should either passed the 12th exam or appearing in that particular year in which he/she is going to give exam\n
+<span style = "color : white; font-weight: bold; font-size: larger;" class="underline"> List of Qualifying Exam: </span>\n\t
+• The final examination of the 10+2 system, conducted by any recognized  Central/ State Board, such as the Central Board of Secondary Education, New Delhi; Council for the Indian School Certificate Examinations, New Delhi; etc.\n\t
+• Intermediate or two-year Pre-University examination conducted by a recognized Board/University.\n\t
+• Final examination of the two-year course of the Joint Services Wing of the National Defense Academy\n\t
+• Senior Secondary School Examination conducted by the National Institute of Open Schooling with a minimum of five subjects.\n\t
+• Any Public School/ Board/ University examination in India or any foreign country is recognized as equivalent to the 10+2 system by the Association of Indian Universities (AIU).\n\t
+• Higher Secondary Certificate Vocational Examination.\n\t
+• A Diploma recognized by AICTE or a State board of technical education of at least 3 years duration.\n\t
+• General Certificate Education (GCE) examination (London/Cambridge/Sri Lanka) at the Advanced (A) level.\n\t
+• High School Certificate Examination of the Cambridge University or International Baccalaureate Diploma of the International Baccalaureate Office, Geneva.\n\t
+• Candidates who have completed the Class XII (or equivalent) examination outside India or from a Board not specified above should produce a certificate from the Association of Indian Universities (AIU) to the effect that the examination they have passed is equivalent to the Class XII Examination.\n\t
+• In case the Class XII Examination is not a public examination,the candidate must have passed at least one public (Board or Pre-University) examination earlier\n`,
     
     
     
-    
-    `State code of eligibility means the code of the State from where the candidate has passed the Class XII (or equivalent) qualifying examination by virtue of which the candidate becomes eligible to appear in JEE (Main) - 2025. It is important to note that the State code of eligibility does NOT depend upon the native place or the place of residence of the candidate. For example, if a candidate appears for the Class XII(or equivalent) qualifying examination from an Institution situated in New Delhi and is a resident of Noida, Uttar Pradesh, then the candidate's State code of eligibility will be Delhi and NOT Uttar Pradesh. 
-    If a candidate has passed the Class XII (or equivalent) qualifying examination from one State but appeared for improvement from another State, the candidate's State code of eligibility will be from where the candidate first passed the Class XII (or equivalent) examination and NOT the State from where the candidate has appeared for improvement. Candidates passed / appearing for Class XII from NIOS should select the State of Eligibility according to the State in which the study Centre is located. For Indian nationals passing the Class XII (or equivalent) examination from Nepal/Bhutan, the State code of eligibility will be determined based on a permanent address in India as given in the passport of the candidate. The State code of eligibility for OCI passing the Class XII (or equivalent) examination in India is at par with Indian nationals. 
-    However, OCI passing the Class XII (or equivalent) examination from an institution abroad are eligible for Other State quota seats or All India quota seats (but NOT for Home State quota seats) in all NITs, IIITs and Other-CFTIs.`,
+`For appearing in the JEE (Main) - 2025, there is no age limit for the candidates. The candidates who have passed the class XII / equivalent examination in 2023, 2024 or appearing in 2025 irrespective of their age can appear in JEE (Main) - 2025 examination. However, the candidates may be required to fulfill the age criteria of the Institute(s) to which they are desirous of taking admission.`,
     
     
     
-    `<span style = "color : blue;"><a href="https://jeemain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a></span> to download jee main latest syllabus`,
+    // State of Eligibility
+`State code of eligibility means the code of the State from where the candidate has passed the Class XII (or equivalent) qualifying examination by virtue of which the candidate becomes eligible to appear in JEE (Main) - 2025. It is important to note that the State code of eligibility does NOT depend upon the native place or the place of residence of the candidate. For example, if a candidate appears for the Class XII(or equivalent) qualifying examination from an Institution situated in New Delhi and is a resident of Noida, Uttar Pradesh, then the candidate's State code of eligibility will be Delhi and NOT Uttar Pradesh. 
+If a candidate has passed the Class XII (or equivalent) qualifying examination from one State but appeared for improvement from another State, the candidate's State code of eligibility will be from where the candidate first passed the Class XII (or equivalent) examination and NOT the State from where the candidate has appeared for improvement. Candidates passed / appearing for Class XII from NIOS should select the State of Eligibility according to the State in which the study Centre is located. For Indian nationals passing the Class XII (or equivalent) examination from Nepal/Bhutan, the State code of eligibility will be determined based on a permanent address in India as given in the passport of the candidate. The State code of eligibility for OCI passing the Class XII (or equivalent) examination in India is at par with Indian nationals. 
+However, OCI passing the Class XII (or equivalent) examination from an institution abroad are eligible for Other State quota seats or All India quota seats (but NOT for Home State quota seats) in all NITs, IIITs and Other-CFTIs.`,
     
     
+// Syllabus
+`<span style ="color : white;" class="underline""><a href="https://jeemain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a></span> to download JEE main latest syllabus`,
     
-    `<span style = "color : blue;"><a href="https://jeemain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a></span> to download jee main latest result`,
+    
+// Admit card
+`The e-Admit Card will be issued provisionally to the candidates through the NTA website: <span style ="color : white;" class="underline""><a href="https://jeemain.nta.nic.in/" target="_blank" rel="noopener noreferrer">https://jeemain.nta.nic.in/</a></span>, subject to the fulfillment of the eligibility conditions and receipt of the
+prescribed application fee by NTA. The candidate has to download the Admit Card from the NTA
+website. The candidate will appear in the examination at the given Centre on the date and
+shift/timing as indicated in his/her e-Admit Card. No candidate will be allowed to appear at the
+examination Centre, on the date and shift/time other than that allotted to him/ her in his/ her
+Admit Card.\n
+JEE Main 2025 admit card will be available 3 days before the JEE main exam`,
     
     
+// JEE Main Reservation
+`Indian nationals belonging to certain categories are admitted under the seats reserved for them
+in accordance with the rules prescribed by the Government of India. The categories and the
+extent of reservation are as follows:\n
+\t\t1. General category belonging to the Economically Weaker Section (GEN- EWS) - 10% of seats in every course. The benefit of reservation will be given only to those General category candidates who satisfy the conditions given in the OM No. 20013/01/2018-BC-II dated 17 January 2019, issued by the Ministry of Social Justice and Empowerment. The criteria for GEN-EWS will be as per the prevailing norms and/or notifications of the Government of India.\n\t
+
+\t\t2. Other Backward Classes belonging to the Non-Creamy Layer (OBC- NCL) - 27% of
+seats in every course. a) OBCs should be listed in the current updated central list of OBCs (http://www.ncbc.ac.in). b) OBCs present in the State list but not covered in the central list of OBCs (as per the list in http://www.ncbc.ac.in) are NOT eligible to claim the reservation. c) The criteria for OBC-NCL will be as per the notification of the Government of India. d) Candidates belonging to the creamy layer of OBC are NOT entitled to reservation. Such candidates are treated as belonging to the general (GEN), i.e. unreserved category, and they will be eligible only for the OPEN seats - the seats for which all candidates are eligible.\n\t
+
+\t3. Scheduled Caste (SC) - 15% of seats in every course.\n\t
+
+\t4. Scheduled Tribe (ST) - 7.5% of seats in every course.\n\t
+
+\t5. Persons with Disability/ Persons with Benchmark Disability (PwD/PwBD) - 5% seats in each of the GENERAL, GEN-EWS, OBC- NCL, SC and ST category seats.`,
     
-    `<span style = "color : blue;"><a href="https://jeemain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a></span> to download jee main latest marks vs percentile`,
-    `Jee counselling process will consist of two step\n
-    • JOSAA\n
-    • CSAB\n`,
-    
+
+// JEE Main Results
+`<span style ="color : white;" class="underline""><a href="JEEmain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a> </span> to get JEE Main Result.`,
+
+// JEE Main Marks vs Percentile
+`<span style ="color : white;" class="underline""><a href="JEEmain.nta.nic.in" target="_blank" rel="noopener noreferrer">Click here</a> </span> to watch marks vs percentile for JEE main 2025(here we will add the youtube video
+link)`,
+
+
+// JEE Main Counselling Process
+`JEE Counselling process will consist of two step\n
+\ta):JOSAA (ispe clicke krne ke bad josaa wala page open hoga)\n
+\tB:)CSAB (ispe click krne ke bad csab wala page open hoga)`
 ]
 
 
