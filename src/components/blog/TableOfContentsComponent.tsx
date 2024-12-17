49e7957a751ba2cf72dba69f3c5ae85c9c970ae1
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-12-17 20:38:52 +0530

    Fix : Fixed the gap that was coming earlier in the table of contents when we add multiple images on the same blog component

--- src/components/blog/TableOfContentsComponent.tsx
+++ src/components/blog/TableOfContentsComponent.tsx
@@ -10,15 +10,24 @@ const TableOfContentsComponent = (props : ITableOfContent) => {
 
                 {/* here we will have to use the map in order to show the table of contents dynamically  */}
                 {props.headingIdList.map((_ : string, index : number) => (
-                    <a
-                        href={`#${props.headingIdList[index]}`}
-                        className="block px-4 py-2 text-sm text-slate-300 hover:bg-gray-100 hover:text-black hover:rounded-lg"
-                        role="menuitem"
-                    >
-                        {props.headingNameList[index]}
-                    </a>
-
-                ))}
+                    props.headingNameList[index] == ""? null :  /**
+                    * After getting the request from client side about adding the support of multiple images per blog section
+                    * We have to add "" empty string in order to add the support of multiple images 
+                    * Hence now heading name list can be an empty string.Due to which empty gap is coming in the table of contents
+                    * If the heading string is an empty string then we will not show anything and hence will remove the gap from TOC
+                    * using the if else statement on the heading name we have to show the html other wise keep the html content as null itself
+                    */
+                   
+                   <a
+                       href={`#${props.headingIdList[index]}`}
+                       className="block px-4 py-2 text-sm text-slate-300 hover:bg-gray-100 hover:text-black hover:rounded-lg"
+                       role="menuitem"
+                   >
+                       {props.headingNameList[index]}
+                   </a>
+)
+                    
+                )}
             </ul>
         </div>
     )
