2086a1e13e1e429ab74e14f6c5dd251ec9894e85
Rupesh Kumar <therupeshkumar8825@gmail.com>
2024-12-27 22:05:54 +0530

    Feature : Testimonial page made responsive now

--- src/home/components/TestimonialComponent.tsx
+++ src/home/components/TestimonialComponent.tsx
@@ -22,13 +22,12 @@ const TestimonialComponent = () => {
 // Here comes the single component for the testimonial component
 const TestimonialBoxComponent = () => {
     return (
-        <div className="w-1/3 ml-10 mb-8 border border-gray-200 rounded-xl shadow-sm border-gray-700 md:mb-12 bg-gray-800">
+        <div className="w-full md:w-1/3 ml-0 md:ml-10 mb-8 border border-gray-200 rounded-xl shadow-sm border-gray-700 md:mb-12 bg-gray-800">
             <figure className="flex flex-col items-center justify-center p-8 text-center  border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e bg-gray-800 border-gray-700">
                 <blockquote className="max-w-2xl mx-auto mb-4 text-gray-300 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-white">Very easy this was to integrate</h3>
                     <p className="my-4">If you care for your time, I hands down would go with this." 
-                        And more texts just for testing purpose so that we can get the given values this
-                         purpose. These are just random texts</p>
+                        And more texts just for </p>
                 </blockquote>
                 <figcaption className="flex items-center justify-center ">
                     <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture"/>
