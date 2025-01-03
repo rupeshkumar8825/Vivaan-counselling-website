4b667a45de2f3ac3f15e21c822840ace91959d9b
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-03 22:23:00 +0530

    Wip: Add constants for the seat section for all the NIT colleges

--- src/constants/college/NIT/NITSeatDataCollegeConstant.tsx
+++ src/constants/college/NIT/NITSeatDataCollegeConstant.tsx
@@ -12,10 +12,565 @@ export const NITWarangalSeatDataTableConstant = [
     ["Sc-PwD", "10", "0"],
     ["St-PwD", "2", "0"]
 ]
-
-
-
 export const NITWarangalSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITBhopalSeatDataTableConstant = [
+    ["General", "300", "80"],
+    ["Ews", "70", "15"],
+    ["Obc-Ncl", "200", "45"],
+    ["Sc", "100", "30"],
+    ["St", "50", "15"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "8", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITBhopalSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITNagpurSeatDataTableConstant = [
+    ["General", "320", "90"],
+    ["Ews", "85", "18"],
+    ["Obc-Ncl", "210", "50"],
+    ["Sc", "110", "35"],
+    ["St", "55", "18"],
+    ["General-PwD", "12", "3"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "11", "6"],
+    ["Sc-PwD", "9", "1"],
+    ["St-PwD", "3", "0"]
+]
+export const NITNagpurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITDurgapurSeatDataTableConstant = [
+    ["General", "310", "85"],
+    ["Ews", "80", "16"],
+    ["Obc-Ncl", "205", "48"],
+    ["Sc", "108", "32"],
+    ["St", "54", "16"],
+    ["General-PwD", "11", "2"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "9", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITDurgapurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITJamshedpurSeatDataTableConstant = [
+    ["General", "305", "83"],
+    ["Ews", "75", "15"],
+    ["Obc-Ncl", "202", "46"],
+    ["Sc", "106", "31"],
+    ["St", "52", "15"],
+    ["General-PwD", "13", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "11", "5"],
+    ["Sc-PwD", "8", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITJamshedpurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITKarnatakaSeatDataTableConstant = [
+    ["General", "325", "88"],
+    ["Ews", "82", "17"],
+    ["Obc-Ncl", "215", "49"],
+    ["Sc", "112", "33"],
+    ["St", "56", "17"],
+    ["General-PwD", "14", "3"],
+    ["Ews-PwD", "10", "2"],
+    ["Obc-Ncl-PwD", "12", "6"],
+    ["Sc-PwD", "10", "0"],
+    ["St-PwD", "3", "0"]
+]
+export const NITKarnatakaSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITSrinagarSeatDataTableConstant = [
+    ["General", "290", "75"],
+    ["Ews", "70", "14"],
+    ["Obc-Ncl", "190", "42"],
+    ["Sc", "100", "28"],
+    ["St", "50", "14"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "9", "4"],
+    ["Sc-PwD", "7", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITSrinagarSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITAllahabadSeatDataTableConstant = [
+    ["General", "330", "92"],
+    ["Ews", "85", "18"],
+    ["Obc-Ncl", "220", "52"],
+    ["Sc", "115", "36"],
+    ["St", "58", "18"],
+    ["General-PwD", "15", "3"],
+    ["Ews-PwD", "10", "2"],
+    ["Obc-Ncl-PwD", "12", "6"],
+    ["Sc-PwD", "10", "0"],
+    ["St-PwD", "3", "0"]
+]
+export const NITAllahabadSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITSuratSeatDataTableConstant = [
+    ["General", "315", "87"],
+    ["Ews", "80", "17"],
+    ["Obc-Ncl", "210", "48"],
+    ["Sc", "110", "34"],
+    ["St", "55", "17"],
+    ["General-PwD", "13", "2"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "11", "5"],
+    ["Sc-PwD", "9", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITSuratSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITCalicutSeatDataTableConstant = [
+    ["General", "340", "95"],
+    ["Ews", "90", "19"],
+    ["Obc-Ncl", "230", "54"],
+    ["Sc", "120", "38"],
+    ["St", "60", "19"],
+    ["General-PwD", "16", "3"],
+    ["Ews-PwD", "11", "2"],
+    ["Obc-Ncl-PwD", "13", "6"],
+    ["Sc-PwD", "11", "0"],
+    ["St-PwD", "3", "0"]
+]
+export const NITCalicutSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITRourkelaSeatDataTableConstant = [
+    ["General", "325", "88"],
+    ["Ews", "82", "17"],
+    ["Obc-Ncl", "215", "49"],
+    ["Sc", "112", "33"],
+    ["St", "56", "17"],
+    ["General-PwD", "14", "3"],
+    ["Ews-PwD", "10", "2"],
+    ["Obc-Ncl-PwD", "12", "6"],
+    ["Sc-PwD", "10", "0"],
+    ["St-PwD", "3", "0"]
+]
+export const NITRourkelaSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITJaipurSeatDataTableConstant = [
+    ["General", "320", "90"],
+    ["Ews", "85", "18"],
+    ["Obc-Ncl", "210", "50"],
+    ["Sc", "110", "35"],
+    ["St", "55", "18"],
+    ["General-PwD", "12", "3"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "11", "6"],
+    ["Sc-PwD", "9", "1"],
+    ["St-PwD", "3", "0"]
+]
+export const NITJaipurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITKurukshetraSeatDataTableConstant = [
+    ["General", "310", "85"],
+    ["Ews", "80", "16"],
+    ["Obc-Ncl", "205", "48"],
+    ["Sc", "108", "32"],
+    ["St", "54", "16"],
+    ["General-PwD", "11", "2"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "9", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITKurukshetraSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITTiruchirapalliSeatDataTableConstant = [
+    ["General", "335", "93"],
+    ["Ews", "87", "18"],
+    ["Obc-Ncl", "225", "53"],
+    ["Sc", "118", "37"],
+    ["St", "59", "18"],
+    ["General-PwD", "15", "3"],
+    ["Ews-PwD", "10", "2"],
+    ["Obc-Ncl-PwD", "12", "6"],
+    ["Sc-PwD", "10", "0"],
+    ["St-PwD", "3", "0"]
+]
+export const NITTiruchirapalliSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITSilcharSeatDataTableConstant = [
+    ["General", "300", "80"],
+    ["Ews", "75", "15"],
+    ["Obc-Ncl", "200", "45"],
+    ["Sc", "100", "30"],
+    ["St", "50", "15"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "8", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITSilcharSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITHamirpurSeatDataTableConstant = [
+    ["General", "310", "85"],
+    ["Ews", "80", "16"],
+    ["Obc-Ncl", "205", "48"],
+    ["Sc", "108", "32"],
+    ["St", "54", "16"],
+    ["General-PwD", "11", "2"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "9", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITHamirpurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITJalandharSeatDataTableConstant = [
+    ["General", "320", "90"],
+    ["Ews", "85", "18"],
+    ["Obc-Ncl", "210", "50"],
+    ["Sc", "110", "35"],
+    ["St", "55", "18"],
+    ["General-PwD", "12", "3"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "11", "6"],
+    ["Sc-PwD", "9", "1"],
+    ["St-PwD", "3", "0"]
+]
+export const NITJalandharSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITPatnaSeatDataTableConstant = [
+    ["General", "305", "83"],
+    ["Ews", "75", "15"],
+    ["Obc-Ncl", "202", "46"],
+    ["Sc", "106", "31"],
+    ["St", "52", "15"],
+    ["General-PwD", "13", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "11", "5"],
+    ["Sc-PwD", "8", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITPatnaSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITRaipurSeatDataTableConstant = [
+    ["General", "315", "87"],
+    ["Ews", "80", "17"],
+    ["Obc-Ncl", "210", "48"],
+    ["Sc", "110", "34"],
+    ["St", "55", "17"],
+    ["General-PwD", "13", "2"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "11", "5"],
+    ["Sc-PwD", "9", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITRaipurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITAgaratalaSeatDataTableConstant = [
+    ["General", "290", "75"],
+    ["Ews", "70", "14"],
+    ["Obc-Ncl", "190", "42"],
+    ["Sc", "100", "28"],
+    ["St", "50", "14"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "9", "4"],
+    ["Sc-PwD", "7", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITAgaratalaSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITArunachalPradeshSeatDataTableConstant = [
+    ["General", "280", "70"],
+    ["Ews", "65", "13"],
+    ["Obc-Ncl", "180", "40"],
+    ["Sc", "95", "26"],
+    ["St", "45", "13"],
+    ["General-PwD", "9", "2"],
+    ["Ews-PwD", "7", "2"],
+    ["Obc-Ncl-PwD", "8", "4"],
+    ["Sc-PwD", "6", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITArunachalPradeshSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITDelhiSeatDataTableConstant = [
+    ["General", "300", "80"],
+    ["Ews", "75", "15"],
+    ["Obc-Ncl", "200", "45"],
+    ["Sc", "100", "30"],
+    ["St", "50", "15"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "8", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITDelhiSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITGoaSeatDataTableConstant = [
+    ["General", "290", "75"],
+    ["Ews", "70", "14"],
+    ["Obc-Ncl", "190", "42"],
+    ["Sc", "100", "28"],
+    ["St", "50", "14"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "9", "4"],
+    ["Sc-PwD", "7", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITGoaSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITManipurSeatDataTableConstant = [
+    ["General", "280", "70"],
+    ["Ews", "65", "13"],
+    ["Obc-Ncl", "180", "40"],
+    ["Sc", "95", "26"],
+    ["St", "45", "13"],
+    ["General-PwD", "9", "2"],
+    ["Ews-PwD", "7", "2"],
+    ["Obc-Ncl-PwD", "8", "4"],
+    ["Sc-PwD", "6", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITManipurSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITMeghalayaSeatDataTableConstant = [
+    ["General", "275", "68"],
+    ["Ews", "65", "13"],
+    ["Obc-Ncl", "175", "39"],
+    ["Sc", "90", "25"],
+    ["St", "45", "13"],
+    ["General-PwD", "9", "2"],
+    ["Ews-PwD", "7", "2"],
+    ["Obc-Ncl-PwD", "8", "4"],
+    ["Sc-PwD", "6", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITMeghalayaSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITMizoramSeatDataTableConstant = [
+    ["General", "270", "67"],
+    ["Ews", "65", "13"],
+    ["Obc-Ncl", "170", "38"],
+    ["Sc", "90", "25"],
+    ["St", "45", "13"],
+    ["General-PwD", "9", "2"],
+    ["Ews-PwD", "7", "2"],
+    ["Obc-Ncl-PwD", "8", "4"],
+    ["Sc-PwD", "6", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITMizoramSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITNagalandSeatDataTableConstant = [
+    ["General", "265", "65"],
+    ["Ews", "60", "12"],
+    ["Obc-Ncl", "165", "37"],
+    ["Sc", "85", "24"],
+    ["St", "45", "13"],
+    ["General-PwD", "8", "2"],
+    ["Ews-PwD", "7", "2"],
+    ["Obc-Ncl-PwD", "8", "4"],
+    ["Sc-PwD", "6", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITNagalandSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITPuducherrySeatDataTableConstant = [
+    ["General", "295", "74"],
+    ["Ews", "70", "14"],
+    ["Obc-Ncl", "185", "41"],
+    ["Sc", "95", "27"],
+    ["St", "50", "14"],
+    ["General-PwD", "9", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "9", "4"],
+    ["Sc-PwD", "7", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITPuducherrySeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITSikkimSeatDataTableConstant = [
+    ["General", "285", "72"],
+    ["Ews", "68", "14"],
+    ["Obc-Ncl", "180", "40"],
+    ["Sc", "90", "26"],
+    ["St", "45", "13"],
+    ["General-PwD", "9", "2"],
+    ["Ews-PwD", "7", "2"],
+    ["Obc-Ncl-PwD", "8", "4"],
+    ["Sc-PwD", "6", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITSikkimSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITUttarakhandSeatDataTableConstant = [
+    ["General", "300", "80"],
+    ["Ews", "75", "15"],
+    ["Obc-Ncl", "200", "45"],
+    ["Sc", "100", "30"],
+    ["St", "50", "15"],
+    ["General-PwD", "10", "2"],
+    ["Ews-PwD", "8", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "8", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITUttarakhandSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITAndhraPradeshSeatDataTableConstant = [
+    ["General", "310", "85"],
+    ["Ews", "80", "16"],
+    ["Obc-Ncl", "205", "48"],
+    ["Sc", "108", "32"],
+    ["St", "54", "16"],
+    ["General-PwD", "11", "2"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "10", "5"],
+    ["Sc-PwD", "9", "0"],
+    ["St-PwD", "2", "0"]
+]
+export const NITAndhraPradeshSeatDataTableHeading = [
+    "Category", 
+    "Total Seat Matrix(Gender-Neutral)", 
+    "Total Seat Matrix(Female Only)"
+]
+
+export const NITShibpurSeatDataTableConstant = [
+    ["General", "320", "90"],
+    ["Ews", "85", "18"],
+    ["Obc-Ncl", "210", "50"],
+    ["Sc", "110", "35"],
+    ["St", "55", "18"],
+    ["General-PwD", "12", "3"],
+    ["Ews-PwD", "9", "2"],
+    ["Obc-Ncl-PwD", "11", "6"],
+    ["Sc-PwD", "9", "1"],
+    ["St-PwD", "3", "0"]
+]
+export const NITShibpurSeatDataTableHeading = [
     "Category", 
     "Total Seat Matrix(Gender-Neutral)", 
     "Total Seat Matrix(Female Only)"
