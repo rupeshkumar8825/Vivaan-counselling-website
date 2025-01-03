9d436785086db78c02d931f8e32d8d22ee13f82f
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-03 22:32:23 +0530

    Wip : Add placement related data for all nit colleges

--- src/constants/college/NIT/NITPlacementDataCollegeComponent.tsx
+++ src/constants/college/NIT/NITPlacementDataCollegeComponent.tsx
@@ -1,9 +1,228 @@
 // this stores the data about the placement section for all the NITs for this purpose
 import NITWarangalPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
 import NITWarangalPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITBhopalPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITBhopalPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITNagpurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITNagpurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITDurgapurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITDurgapurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITJamshedpurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITJamshedpurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITKarnatakaPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITKarnatakaPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
 
 
 export const NITWarangalPlacementImageList = [
     NITWarangalPlacementImage1, 
     NITWarangalPlacementImage2
+]
+
+export const NITBhopalPlacementImageList = [
+    NITBhopalPlacementImage1, 
+    NITBhopalPlacementImage2
+]
+
+export const NITNagpurPlacementImageList = [
+    NITNagpurPlacementImage1, 
+    NITNagpurPlacementImage2
+]
+
+export const NITDurgapurPlacementImageList = [
+    NITDurgapurPlacementImage1, 
+    NITDurgapurPlacementImage2
+]
+
+export const NITJamshedpurPlacementImageList = [
+    NITJamshedpurPlacementImage1, 
+    NITJamshedpurPlacementImage2
+]
+
+export const NITKarnatakaPlacementImageList = [
+    NITKarnatakaPlacementImage1, 
+    NITKarnatakaPlacementImage2
+]
+
+import NITSrinagarPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITSrinagarPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITAllahabadPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITAllahabadPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITSuratPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITSuratPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITCalicutPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITCalicutPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITRourkelaPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITRourkelaPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITJaipurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITJaipurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITKurukshetraPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITKurukshetraPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITTiruchirapalliPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITTiruchirapalliPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+
+export const NITSrinagarPlacementImageList = [
+    NITSrinagarPlacementImage1, 
+    NITSrinagarPlacementImage2
+]
+
+export const NITAllahabadPlacementImageList = [
+    NITAllahabadPlacementImage1, 
+    NITAllahabadPlacementImage2
+]
+
+export const NITSuratPlacementImageList = [
+    NITSuratPlacementImage1, 
+    NITSuratPlacementImage2
+]
+
+export const NITCalicutPlacementImageList = [
+    NITCalicutPlacementImage1, 
+    NITCalicutPlacementImage2
+]
+
+export const NITRourkelaPlacementImageList = [
+    NITRourkelaPlacementImage1, 
+    NITRourkelaPlacementImage2
+]
+
+export const NITJaipurPlacementImageList = [
+    NITJaipurPlacementImage1, 
+    NITJaipurPlacementImage2
+]
+
+export const NITKurukshetraPlacementImageList = [
+    NITKurukshetraPlacementImage1, 
+    NITKurukshetraPlacementImage2
+]
+
+export const NITTiruchirapalliPlacementImageList = [
+    NITTiruchirapalliPlacementImage1, 
+    NITTiruchirapalliPlacementImage2
+]
+import NITSilcharPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITSilcharPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITHamirpurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITHamirpurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITJalandharPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITJalandharPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITPatnaPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITPatnaPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITRaipurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITRaipurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITAgaratalaPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITAgaratalaPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITArunachalPradeshPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITArunachalPradeshPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+
+export const NITSilcharPlacementImageList = [
+    NITSilcharPlacementImage1, 
+    NITSilcharPlacementImage2
+]
+
+export const NITHamirpurPlacementImageList = [
+    NITHamirpurPlacementImage1, 
+    NITHamirpurPlacementImage2
+]
+
+export const NITJalandharPlacementImageList = [
+    NITJalandharPlacementImage1, 
+    NITJalandharPlacementImage2
+]
+
+export const NITPatnaPlacementImageList = [
+    NITPatnaPlacementImage1, 
+    NITPatnaPlacementImage2
+]
+
+export const NITRaipurPlacementImageList = [
+    NITRaipurPlacementImage1, 
+    NITRaipurPlacementImage2
+]
+
+export const NITAgaratalaPlacementImageList = [
+    NITAgaratalaPlacementImage1, 
+    NITAgaratalaPlacementImage2
+]
+
+export const NITArunachalPradeshPlacementImageList = [
+    NITArunachalPradeshPlacementImage1, 
+    NITArunachalPradeshPlacementImage2
+]
+
+import NITDelhiPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITDelhiPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITGoaPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITGoaPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITManipurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITManipurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITMeghalayaPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITMeghalayaPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITMizoramPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITMizoramPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITNagalandPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITNagalandPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITPuducherryPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITPuducherryPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITSikkimPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITSikkimPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITUttarakhandPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITUttarakhandPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITAndhraPradeshPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITAndhraPradeshPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import NITShibpurPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import NITShibpurPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+
+export const NITDelhiPlacementImageList = [
+    NITDelhiPlacementImage1, 
+    NITDelhiPlacementImage2
+]
+
+export const NITGoaPlacementImageList = [
+    NITGoaPlacementImage1, 
+    NITGoaPlacementImage2
+]
+
+export const NITManipurPlacementImageList = [
+    NITManipurPlacementImage1, 
+    NITManipurPlacementImage2
+]
+
+export const NITMeghalayaPlacementImageList = [
+    NITMeghalayaPlacementImage1, 
+    NITMeghalayaPlacementImage2
+]
+
+export const NITMizoramPlacementImageList = [
+    NITMizoramPlacementImage1, 
+    NITMizoramPlacementImage2
+]
+
+export const NITNagalandPlacementImageList = [
+    NITNagalandPlacementImage1, 
+    NITNagalandPlacementImage2
+]
+
+export const NITPuducherryPlacementImageList = [
+    NITPuducherryPlacementImage1, 
+    NITPuducherryPlacementImage2
+]
+
+export const NITSikkimPlacementImageList = [
+    NITSikkimPlacementImage1, 
+    NITSikkimPlacementImage2
+]
+
+export const NITUttarakhandPlacementImageList = [
+    NITUttarakhandPlacementImage1, 
+    NITUttarakhandPlacementImage2
+]
+
+export const NITAndhraPradeshPlacementImageList = [
+    NITAndhraPradeshPlacementImage1, 
+    NITAndhraPradeshPlacementImage2
+]
+
+export const NITShibpurPlacementImageList = [
+    NITShibpurPlacementImage1, 
+    NITShibpurPlacementImage2
 ]
