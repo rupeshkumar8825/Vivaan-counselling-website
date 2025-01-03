c586ef009cd1afbfda6658a57617d5a1285106d3
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-03 21:42:47 +0530

    Wip: Add constants related to about section for all the NIT colleges

--- src/constants/college/NIT/NITAboutDataCollegeConstant.tsx
+++ src/constants/college/NIT/NITAboutDataCollegeConstant.tsx
@@ -29,3 +29,872 @@ export const NITWarangalFeesDataTableHeading = [
 ]
 
 
+export const NITBhopalAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "MANIT, Maulana Azad National Institute of Technology"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1960"
+    ], 
+    [
+        "Location", 
+        "Bhopal, Madhya Pradesh"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITBhopalFeesDataTableHeading = [
+    "Institute Name", 
+    "Maulana Azad National Institute of Technology, Bhopal"
+]
+
+export const NITNagpurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "VNIT, Visvesvaraya National Institute of Technology"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1960"
+    ], 
+    [
+        "Location", 
+        "Nagpur, Maharashtra"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITNagpurFeesDataTableHeading = [
+    "Institute Name", 
+    "Visvesvaraya National Institute of Technology, Nagpur"
+]
+
+export const NITDurgapurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITD, National Institute of Technology Durgapur"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1960"
+    ], 
+    [
+        "Location", 
+        "Durgapur, West Bengal"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITDurgapurFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Durgapur"
+]
+
+export const NITJamshedpurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITJSR, National Institute of Technology Jamshedpur"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1960"
+    ], 
+    [
+        "Location", 
+        "Jamshedpur, Jharkhand"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITJamshedpurFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Jamshedpur"
+]
+
+export const NITKarnatakaAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITK, National Institute of Technology Karnataka"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1960"
+    ], 
+    [
+        "Location", 
+        "Surathkal, Karnataka"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITKarnatakaFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Karnataka"
+]
+
+
+export const NITSrinagarAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITSRI, National Institute of Technology Srinagar"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1960"
+    ], 
+    [
+        "Location", 
+        "Srinagar, Jammu and Kashmir"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITSrinagarFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Srinagar"
+]
+
+export const NITAllahabadAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "MNNIT, Motilal Nehru National Institute of Technology"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1961"
+    ], 
+    [
+        "Location", 
+        "Allahabad, Uttar Pradesh"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITAllahabadFeesDataTableHeading = [
+    "Institute Name", 
+    "Motilal Nehru National Institute of Technology, Allahabad"
+]
+
+export const NITSuratAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "SVNIT, Sardar Vallabhbhai National Institute of Technology"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1961"
+    ], 
+    [
+        "Location", 
+        "Surat, Gujarat"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITSuratFeesDataTableHeading = [
+    "Institute Name", 
+    "Sardar Vallabhbhai National Institute of Technology, Surat"
+]
+
+export const NITCalicutAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITC, National Institute of Technology Calicut"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1961"
+    ], 
+    [
+        "Location", 
+        "Calicut, Kerala"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITCalicutFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Calicut"
+]
+
+export const NITRourkelaAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITRKL, National Institute of Technology Rourkela"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1961"
+    ], 
+    [
+        "Location", 
+        "Rourkela, Odisha"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITRourkelaFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Rourkela"
+]
+
+export const NITJaipurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "MNIT, Malaviya National Institute of Technology"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1963"
+    ], 
+    [
+        "Location", 
+        "Jaipur, Rajasthan"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITJaipurFeesDataTableHeading = [
+    "Institute Name", 
+    "Malaviya National Institute of Technology, Jaipur"
+]
+
+export const NITKurukshetraAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITKKR, National Institute of Technology Kurukshetra"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1963"
+    ], 
+    [
+        "Location", 
+        "Kurukshetra, Haryana"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITKurukshetraFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Kurukshetra"
+]
+
+export const NITTiruchirappalliAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITT, National Institute of Technology Tiruchirappalli"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1964"
+    ], 
+    [
+        "Location", 
+        "Tiruchirappalli, Tamil Nadu"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITTiruchirappalliFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Tiruchirappalli"
+]
+
+
+export const NITSilcharAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITS, National Institute of Technology Silchar"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1967"
+    ], 
+    [
+        "Location", 
+        "Silchar, Assam"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITSilcharFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Silchar"
+]
+
+export const NITHamirpurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITH, National Institute of Technology Hamirpur"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1986"
+    ], 
+    [
+        "Location", 
+        "Hamirpur, Himachal Pradesh"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITHamirpurFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Hamirpur"
+]
+
+export const NITJalandharAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITJ, Dr. B R Ambedkar National Institute of Technology"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1987"
+    ], 
+    [
+        "Location", 
+        "Jalandhar, Punjab"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITJalandharFeesDataTableHeading = [
+    "Institute Name", 
+    "Dr. B R Ambedkar National Institute of Technology, Jalandhar"
+]
+
+export const NITPatnaAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITP, National Institute of Technology Patna"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2004"
+    ], 
+    [
+        "Location", 
+        "Patna, Bihar"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITPatnaFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Patna"
+]
+
+export const NITRaipurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITRR, National Institute of Technology Raipur"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1956"
+    ], 
+    [
+        "Location", 
+        "Raipur, Chhattisgarh"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITRaipurFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Raipur"
+]
+
+export const NITAgaratalaAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITA, National Institute of Technology Agartala"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1965"
+    ], 
+    [
+        "Location", 
+        "Agartala, Tripura"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITAgaratalaFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Agartala"
+]
+
+export const NITArunachalPradeshAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITAP, National Institute of Technology Arunachal Pradesh"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Yupia, Arunachal Pradesh"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITArunachalPradeshFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Arunachal Pradesh"
+]
+
+export const NITDelhiAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITD, National Institute of Technology Delhi"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Delhi"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITDelhiFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Delhi"
+]
+
+export const NITGoaAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITG, National Institute of Technology Goa"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Farmagudi, Goa"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITGoaFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Goa"
+]
+
+export const NITManipurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITMN, National Institute of Technology Manipur"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Imphal, Manipur"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITManipurFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Manipur"
+]
+
+export const NITMeghalayaAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITMGH, National Institute of Technology Meghalaya"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Shillong, Meghalaya"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITMeghalayaFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Meghalaya"
+]
+
+export const NITMizoramAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITMZ, National Institute of Technology Mizoram"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Aizawl, Mizoram"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITMizoramFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Mizoram"
+]
+
+export const NITNagalandAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITN, National Institute of Technology Nagaland"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Dimapur, Nagaland"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITNagalandFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Nagaland"
+]
+
+export const NITPuducherryAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITPY, National Institute of Technology Puducherry"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Karaikal, Puducherry"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITPuducherryFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Puducherry"
+]
+
+export const NITSikkimAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITSKM, National Institute of Technology Sikkim"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Ravangla, Sikkim"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITSikkimFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Sikkim"
+]
+
+export const NITUttarakhandAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITUK, National Institute of Technology Uttarakhand"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2010"
+    ], 
+    [
+        "Location", 
+        "Srinagar, Uttarakhand"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITUttarakhandFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Uttarakhand"
+]
+
+export const NITAndhraPradeshAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "NITAP, National Institute of Technology Andhra Pradesh"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "2015"
+    ], 
+    [
+        "Location", 
+        "Tadepalligudem, Andhra Pradesh"
+    ], 
+    [
+        "Type", 
+        "NIT"
+    ]
+]
+
+export const NITAndhraPradeshFeesDataTableHeading = [
+    "Institute Name", 
+    "National Institute of Technology, Andhra Pradesh"
+]
+
+export const IIESTShibpurAboutDataTableConstant = [
+    [
+        "Also Known as", 
+        "IIEST, Indian Institute of Engineering Science and Technology Shibpur"
+    ], 
+    [
+        "Institute Type", 
+        "Government"
+    ], 
+    [
+        "Established", 
+        "1856"
+    ], 
+    [
+        "Location", 
+        "Shibpur, West Bengal"
+    ], 
+    [
+        "Type", 
+        "IIEST"
+    ]
+]
+
+export const IIESTShibpurFeesDataTableHeading = [
+    "Institute Name", 
+    "Indian Institute of Engineering Science and Technology, Shibpur"
+]
