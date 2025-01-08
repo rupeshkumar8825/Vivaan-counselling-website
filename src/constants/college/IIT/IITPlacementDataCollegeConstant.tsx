b583d3866db603ea45175494935750b28f8cabea
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-08 07:26:39 +0530

    Wip : Add constant for placement section for all IIT colleges

---
+++ src/constants/college/IIT/IITPlacementDataCollegeConstant.tsx
@@ -0,0 +1,166 @@
+// this will store the constant related to the placement section for all IITs
+
+import IITBombayPlacementImage1 from "../../../assets/images/NIT/placements/nit-warangal-1.png"
+import IITBombayPlacementImage2 from "../../../assets/images/NIT/placements/nit-warangal-2.png"
+import IITKharagpurPlacementImage1 from "../../../assets/images/IIT/placements/iit-kharagpur-1.png"
+import IITKharagpurPlacementImage2 from "../../../assets/images/IIT/placements/iit-kharagpur-2.png"
+import IITMadrasPlacementImage1 from "../../../assets/images/IIT/placements/iit-madras-1.png"
+import IITMadrasPlacementImage2 from "../../../assets/images/IIT/placements/iit-madras-2.png"
+import IITKanpurPlacementImage1 from "../../../assets/images/IIT/placements/iit-kanpur-1.png"
+import IITKanpurPlacementImage2 from "../../../assets/images/IIT/placements/iit-kanpur-2.png"
+import IITDelhiPlacementImage1 from "../../../assets/images/IIT/placements/iit-delhi-1.png"
+import IITDelhiPlacementImage2 from "../../../assets/images/IIT/placements/iit-delhi-2.png"
+import IITGuwahatiPlacementImage1 from "../../../assets/images/IIT/placements/iit-guwahati-1.png"
+import IITGuwahatiPlacementImage2 from "../../../assets/images/IIT/placements/iit-guwahati-2.png"
+import IITRoorkeePlacementImage1 from "../../../assets/images/IIT/placements/iit-roorkee-1.png"
+import IITRoorkeePlacementImage2 from "../../../assets/images/IIT/placements/iit-roorkee-2.png"
+import IITRoparPlacementImage1 from "../../../assets/images/IIT/placements/iit-ropar-1.png"
+import IITRoparPlacementImage2 from "../../../assets/images/IIT/placements/iit-ropar-2.png"
+import IITBhubaneshwarPlacementImage1 from "../../../assets/images/IIT/placements/iit-bhubaneshwar-1.png"
+import IITBhubaneshwarPlacementImage2 from "../../../assets/images/IIT/placements/iit-bhubaneshwar-2.png"
+import IITGandhinagarPlacementImage1 from "../../../assets/images/IIT/placements/iit-gandhinagar-1.png"
+import IITGandhinagarPlacementImage2 from "../../../assets/images/IIT/placements/iit-gandhinagar-2.png"
+import IITHyderabadPlacementImage1 from "../../../assets/images/IIT/placements/iit-hyderabad-1.png"
+import IITHyderabadPlacementImage2 from "../../../assets/images/IIT/placements/iit-hyderabad-2.png"
+import IITJodhpurPlacementImage1 from "../../../assets/images/IIT/placements/iit-jodhpur-1.png"
+import IITJodhpurPlacementImage2 from "../../../assets/images/IIT/placements/iit-jodhpur-2.png"
+import IITPatnaPlacementImage1 from "../../../assets/images/IIT/placements/iit-patna-1.png"
+import IITPatnaPlacementImage2 from "../../../assets/images/IIT/placements/iit-patna-2.png"
+import IITIndorePlacementImage1 from "../../../assets/images/IIT/placements/iit-indore-1.png"
+import IITIndorePlacementImage2 from "../../../assets/images/IIT/placements/iit-indore-2.png"
+import IITMandiPlacementImage1 from "../../../assets/images/IIT/placements/iit-mandi-1.png"
+import IITMandiPlacementImage2 from "../../../assets/images/IIT/placements/iit-mandi-2.png"
+import IITBHUPlacementImage1 from "../../../assets/images/IIT/placements/iit-bhu-1.png"
+import IITBHUPlacementImage2 from "../../../assets/images/IIT/placements/iit-bhu-2.png"
+import IITPalakkadPlacementImage1 from "../../../assets/images/IIT/placements/iit-palakkad-1.png"
+import IITPalakkadPlacementImage2 from "../../../assets/images/IIT/placements/iit-palakkad-2.png"
+import IITTirupatiPlacementImage1 from "../../../assets/images/IIT/placements/iit-tirupati-1.png"
+import IITTirupatiPlacementImage2 from "../../../assets/images/IIT/placements/iit-tirupati-2.png"
+import IITDhanbadPlacementImage1 from "../../../assets/images/IIT/placements/iit-dhanbad-1.png"
+import IITDhanbadPlacementImage2 from "../../../assets/images/IIT/placements/iit-dhanbad-2.png"
+import IITBhilaiPlacementImage1 from "../../../assets/images/IIT/placements/iit-bhilai-1.png"
+import IITBhilaiPlacementImage2 from "../../../assets/images/IIT/placements/iit-bhilai-2.png"
+import IITDharwadPlacementImage1 from "../../../assets/images/IIT/placements/iit-dharwad-1.png"
+import IITDharwadPlacementImage2 from "../../../assets/images/IIT/placements/iit-dharwad-2.png"
+import IITJammuPlacementImage1 from "../../../assets/images/IIT/placements/iit-jammu-1.png"
+import IITJammuPlacementImage2 from "../../../assets/images/IIT/placements/iit-jammu-2.png"
+import IITGoaPlacementImage1 from "../../../assets/images/IIT/placements/iit-goa-1.png"
+import IITGoaPlacementImage2 from "../../../assets/images/IIT/placements/iit-goa-2.png"
+
+
+
+export const IITBombayPlacementImageList = [
+    IITBombayPlacementImage1, 
+    IITBombayPlacementImage2
+]
+
+export const IITKharagpurPlacementImageList = [
+    IITKharagpurPlacementImage1, 
+    IITKharagpurPlacementImage2
+]
+
+export const IITMadrasPlacementImageList = [
+    IITMadrasPlacementImage1, 
+    IITMadrasPlacementImage2
+]
+
+export const IITKanpurPlacementImageList = [
+    IITKanpurPlacementImage1, 
+    IITKanpurPlacementImage2
+]
+
+export const IITDelhiPlacementImageList = [
+    IITDelhiPlacementImage1, 
+    IITDelhiPlacementImage2
+]
+
+export const IITGuwahatiPlacementImageList = [
+    IITGuwahatiPlacementImage1, 
+    IITGuwahatiPlacementImage2
+]
+
+export const IITRoorkeePlacementImageList = [
+    IITRoorkeePlacementImage1, 
+    IITRoorkeePlacementImage2
+]
+
+export const IITRoparPlacementImageList = [
+    IITRoparPlacementImage1, 
+    IITRoparPlacementImage2
+]
+
+export const IITBhubaneshwarPlacementImageList = [
+    IITBhubaneshwarPlacementImage1, 
+    IITBhubaneshwarPlacementImage2
+]
+
+export const IITGandhinagarPlacementImageList = [
+    IITGandhinagarPlacementImage1, 
+    IITGandhinagarPlacementImage2
+]
+
+export const IITHyderabadPlacementImageList = [
+    IITHyderabadPlacementImage1, 
+    IITHyderabadPlacementImage2
+]
+
+export const IITJodhpurPlacementImageList = [
+    IITJodhpurPlacementImage1, 
+    IITJodhpurPlacementImage2
+]
+
+export const IITPatnaPlacementImageList = [
+    IITPatnaPlacementImage1, 
+    IITPatnaPlacementImage2
+]
+
+export const IITIndorePlacementImageList = [
+    IITIndorePlacementImage1, 
+    IITIndorePlacementImage2
+]
+
+export const IITMandiPlacementImageList = [
+    IITMandiPlacementImage1, 
+    IITMandiPlacementImage2
+]
+
+export const IITBHUPlacementImageList = [
+    IITBHUPlacementImage1, 
+    IITBHUPlacementImage2
+]
+
+export const IITPalakkadPlacementImageList = [
+    IITPalakkadPlacementImage1, 
+    IITPalakkadPlacementImage2
+]
+
+
+export const IITTirupatiPlacementImageList = [
+    IITTirupatiPlacementImage1, 
+    IITTirupatiPlacementImage2
+]
+
+export const IITDhanbadPlacementImageList = [
+    IITDhanbadPlacementImage1, 
+    IITDhanbadPlacementImage2
+]
+
+export const IITBhilaiPlacementImageList = [
+    IITBhilaiPlacementImage1, 
+    IITBhilaiPlacementImage2
+]
+
+export const IITDharwadPlacementImageList = [
+    IITDharwadPlacementImage1, 
+    IITDharwadPlacementImage2
+]
+
+export const IITJammuPlacementImageList = [
+    IITJammuPlacementImage1, 
+    IITJammuPlacementImage2
+]
+
+export const IITGoaPlacementImageList = [
+    IITGoaPlacementImage1, 
+    IITGoaPlacementImage2
+]
