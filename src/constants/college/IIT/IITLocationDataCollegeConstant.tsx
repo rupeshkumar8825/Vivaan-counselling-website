e05e9d2f52a6844ec915435e0825471530e3c3b6
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-08 07:22:24 +0530

    Wip : Add constant for location section for all IIT colleges

---
+++ src/constants/college/IIT/IITLocationDataCollegeConstant.tsx
@@ -0,0 +1,261 @@
+// this will store the information about the location section for all IITs colleges
+
+export const IITBombayLocationDataTableConstant = [
+    [
+        "Tiruchirappalli International Airport", 
+        "18"
+    ], 
+    [
+        "Tiruchirappalli Junction Railway Station", 
+        "18.5"
+    ]
+]
+
+
+export const IITBombayLocationDataTableHeading = [
+
+]
+
+export const IITKharagpurLocationDataTableConstant = [
+    [
+        "Netaji Subhas Chandra Bose International Airport", 
+        "140"
+    ], 
+    [
+        "Kharagpur Junction Railway Station", 
+        "5"
+    ]
+]
+
+export const IITMadrasLocationDataTableConstant = [
+    [
+        "Chennai International Airport", 
+        "15"
+    ], 
+    [
+        "Chennai Central Railway Station", 
+        "13"
+    ]
+]
+
+export const IITKanpurLocationDataTableConstant = [
+    [
+        "Kanpur Airport", 
+        "16"
+    ], 
+    [
+        "Kanpur Central Railway Station", 
+        "15"
+    ]
+]
+
+export const IITDelhiLocationDataTableConstant = [
+    [
+        "Indira Gandhi International Airport", 
+        "10"
+    ], 
+    [
+        "New Delhi Railway Station", 
+        "15"
+    ]
+]
+
+export const IITGuwahatiLocationDataTableConstant = [
+    [
+        "Lokpriya Gopinath Bordoloi International Airport", 
+        "20"
+    ], 
+    [
+        "Guwahati Railway Station", 
+        "18"
+    ]
+]
+
+export const IITRoorkeeLocationDataTableConstant = [
+    [
+        "Jolly Grant Airport", 
+        "70"
+    ], 
+    [
+        "Roorkee Railway Station", 
+        "3"
+    ]
+]
+
+export const IITRoparLocationDataTableConstant = [
+    [
+        "Chandigarh International Airport", 
+        "60"
+    ], 
+    [
+        "Rupnagar Railway Station", 
+        "5"
+    ]
+]
+
+
+export const IITBhubaneswarLocationDataTableConstant = [
+    [
+        "Biju Patnaik International Airport", 
+        "15"
+    ], 
+    [
+        "Bhubaneswar Railway Station", 
+        "20"
+    ]
+]
+
+export const IITGandhinagarLocationDataTableConstant = [
+    [
+        "Sardar Vallabhbhai Patel International Airport", 
+        "25"
+    ], 
+    [
+        "Ahmedabad Railway Station", 
+        "28"
+    ]
+]
+
+export const IITHyderabadLocationDataTableConstant = [
+    [
+        "Rajiv Gandhi International Airport", 
+        "50"
+    ], 
+    [
+        "Hyderabad Deccan Railway Station", 
+        "35"
+    ]
+]
+
+export const IITJodhpurLocationDataTableConstant = [
+    [
+        "Jodhpur Airport", 
+        "12"
+    ], 
+    [
+        "Jodhpur Junction Railway Station", 
+        "14"
+    ]
+]
+
+export const IITPatnaLocationDataTableConstant = [
+    [
+        "Jay Prakash Narayan International Airport", 
+        "35"
+    ], 
+    [
+        "Patna Junction Railway Station", 
+        "30"
+    ]
+]
+
+export const IITIndoreLocationDataTableConstant = [
+    [
+        "Devi Ahilya Bai Holkar Airport", 
+        "25"
+    ], 
+    [
+        "Indore Junction Railway Station", 
+        "20"
+    ]
+]
+
+export const IITMandiLocationDataTableConstant = [
+    [
+        "Kullu–Manali Airport", 
+        "75"
+    ], 
+    [
+        "Kiratpur Sahib Railway Station", 
+        "125"
+    ]
+]
+
+export const IITBHUVaranasiLocationDataTableConstant = [
+    [
+        "Lal Bahadur Shastri International Airport", 
+        "30"
+    ], 
+    [
+        "Varanasi Junction Railway Station", 
+        "10"
+    ]
+]
+
+export const IITPalakkadLocationDataTableConstant = [
+    [
+        "Coimbatore International Airport", 
+        "60"
+    ], 
+    [
+        "Palakkad Junction Railway Station", 
+        "20"
+    ]
+]
+
+
+export const IITTirupatiLocationDataTableConstant = [
+    [
+        "Tirupati Airport", 
+        "14"
+    ], 
+    [
+        "Tirupati Railway Station", 
+        "10"
+    ]
+]
+
+export const IITDhanbadLocationDataTableConstant = [
+    [
+        "Birsa Munda Airport", 
+        "150"
+    ], 
+    [
+        "Dhanbad Junction Railway Station", 
+        "5"
+    ]
+]
+
+export const IITBhilaiLocationDataTableConstant = [
+    [
+        "Swami Vivekananda Airport", 
+        "45"
+    ], 
+    [
+        "Bhilai Nagar Railway Station", 
+        "10"
+    ]
+]
+
+export const IITDharwadLocationDataTableConstant = [
+    [
+        "Hubli Airport", 
+        "30"
+    ], 
+    [
+        "Dharwad Railway Station", 
+        "12"
+    ]
+]
+
+export const IITJammuLocationDataTableConstant = [
+    [
+        "Jammu Airport", 
+        "15"
+    ], 
+    [
+        "Jammu Tawi Railway Station", 
+        "12"
+    ]
+]
+
+export const IITGoaLocationDataTableConstant = [
+    [
+        "Goa International Airport", 
+        "35"
+    ], 
+    [
+        "Madgaon Railway Station", 
+        "30"
+    ]
+]
