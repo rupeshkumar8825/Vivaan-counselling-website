7fcc7f25c57db14686843ef833996b4fc4cbf48b
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-09 06:55:53 +0530

    Wip : Add IIIT about section constant for all IIITs

---
+++ src/constants/college/IIIT/IIITAboutDataCollegeConstant.tsx
@@ -0,0 +1,366 @@
+// this file will store the constant for about section of the all IIITs colleges 
+
+export const IIITHyderabadAboutDataTableConstant = [
+    ["Also Known as", "IIT KGP"],
+    ["Institute Type", "Government"],
+    ["Established", "1951"],
+    ["Location", "Kharagpur, West Bengal"],
+    ["Type", "IIT"]
+];
+
+export const IIITHyderabadAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Technology, Kharagpur"
+];
+
+
+export const IIITGwaliorAboutDataTableConstant = [
+    ["Also Known as", "ABV-IIITM"],
+    ["Institute Type", "Government"],
+    ["Established", "1997"],
+    ["Location", "Gwalior, Madhya Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITGwaliorAboutDataTableHeading = [
+    "Institute Name",
+    "Atal Bihari Vajpayee Indian Institute of Information Technology and Management, Gwalior"
+];
+
+export const IIITBangaloreAboutDataTableConstant = [
+    ["Also Known as", "IIITB"],
+    ["Institute Type", "Private"],
+    ["Established", "1999"],
+    ["Location", "Bangalore, Karnataka"],
+    ["Type", "IIIT"]
+];
+
+export const IIITBangaloreAboutDataTableHeading = [
+    "Institute Name",
+    "International Institute of Information Technology, Bangalore"
+];
+
+export const IIITGuwahatiAboutDataTableConstant = [
+    ["Also Known as", "IIITG"],
+    ["Institute Type", "Government"],
+    ["Established", "2013"],
+    ["Location", "Guwahati, Assam"],
+    ["Type", "IIIT"]
+];
+
+export const IIITGuwahatiAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Guwahati"
+];
+
+export const IIITJabalpurAboutDataTableConstant = [
+    ["Also Known as", "IIITDMJ"],
+    ["Institute Type", "Government"],
+    ["Established", "2005"],
+    ["Location", "Jabalpur, Madhya Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITJabalpurAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Design and Manufacturing, Jabalpur"
+];
+
+export const IIITAllahabadAboutDataTableConstant = [
+    ["Also Known as", "IIITA"],
+    ["Institute Type", "Government"],
+    ["Established", "1999"],
+    ["Location", "Allahabad, Uttar Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITAllahabadAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Allahabad"
+];
+
+export const IIITKanchipuramAboutDataTableConstant = [
+    ["Also Known as", "IIITDMK"],
+    ["Institute Type", "Government"],
+    ["Established", "2007"],
+    ["Location", "Kanchipuram, Tamil Nadu"],
+    ["Type", "IIIT"]
+];
+
+export const IIITKanchipuramAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Design and Manufacturing, Kanchipuram"
+];
+
+export const IIITBhubaneswarAboutDataTableConstant = [
+    ["Also Known as", "IIITB"],
+    ["Institute Type", "Government"],
+    ["Established", "2006"],
+    ["Location", "Bhubaneswar, Odisha"],
+    ["Type", "IIIT"]
+];
+
+export const IIITBhubaneswarAboutDataTableHeading = [
+    "Institute Name",
+    "International Institute of Information Technology, Bhubaneswar"
+];
+
+export const IIITPuneAboutDataTableConstant = [
+    ["Also Known as", "IIITP"],
+    ["Institute Type", "Government"],
+    ["Established", "2016"],
+    ["Location", "Pune, Maharashtra"],
+    ["Type", "IIIT"]
+];
+
+export const IIITPuneAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Pune"
+];
+
+export const IIITKotaAboutDataTableConstant = [
+    ["Also Known as", "IIITK"],
+    ["Institute Type", "Government"],
+    ["Established", "2013"],
+    ["Location", "Kota, Rajasthan"],
+    ["Type", "IIIT"]
+];
+
+export const IIITKotaAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Kota"
+];
+
+export const IIITSricityAboutDataTableConstant = [
+    ["Also Known as", "IIITS"],
+    ["Institute Type", "Government"],
+    ["Established", "2013"],
+    ["Location", "Sri City, Andhra Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITSricityAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Sri City"
+];
+
+export const IIITVadodaraAboutDataTableConstant = [
+    ["Also Known as", "IIITV"],
+    ["Institute Type", "Government"],
+    ["Established", "2013"],
+    ["Location", "Vadodara, Gujarat"],
+    ["Type", "IIIT"]
+];
+
+export const IIITVadodaraAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Vadodara"
+];
+
+export const IIITNagpurAboutDataTableConstant = [
+    ["Also Known as", "IIITN"],
+    ["Institute Type", "Government"],
+    ["Established", "2016"],
+    ["Location", "Nagpur, Maharashtra"],
+    ["Type", "IIIT"]
+];
+
+export const IIITNagpurAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Nagpur"
+];
+
+export const IIITKalyaniAboutDataTableConstant = [
+    ["Also Known as", "IIITK"],
+    ["Institute Type", "Government"],
+    ["Established", "2014"],
+    ["Location", "Kalyani, West Bengal"],
+    ["Type", "IIIT"]
+];
+
+export const IIITKalyaniAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Kalyani"
+];
+
+export const IIITLucknowAboutDataTableConstant = [
+    ["Also Known as", "IIITL"],
+    ["Institute Type", "Government"],
+    ["Established", "2015"],
+    ["Location", "Lucknow, Uttar Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITLucknowAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Lucknow"
+];
+
+export const IIITDharwadAboutDataTableConstant = [
+    ["Also Known as", "IIITD"],
+    ["Institute Type", "Government"],
+    ["Established", "2015"],
+    ["Location", "Dharwad, Karnataka"],
+    ["Type", "IIIT"]
+];
+
+export const IIITDharwadAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Dharwad"
+];
+
+export const IIITBhagalpurAboutDataTableConstant = [
+    ["Also Known as", "IIITBGP"],
+    ["Institute Type", "Government"],
+    ["Established", "2017"],
+    ["Location", "Bhagalpur, Bihar"],
+    ["Type", "IIIT"]
+];
+
+export const IIITBhagalpurAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Bhagalpur"
+];
+
+export const IIITBhopalAboutDataTableConstant = [
+    ["Also Known as", "IIITBPL"],
+    ["Institute Type", "Government"],
+    ["Established", "2017"],
+    ["Location", "Bhopal, Madhya Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITBhopalAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Bhopal"
+];
+
+export const IIITKottayamAboutDataTableConstant = [
+    ["Also Known as", "IIITK"],
+    ["Institute Type", "Government"],
+    ["Established", "2015"],
+    ["Location", "Kottayam, Kerala"],
+    ["Type", "IIIT"]
+];
+
+export const IIITKottayamAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Kottayam"
+];
+
+export const IIITRanchiAboutDataTableConstant = [
+    ["Also Known as", "IIITR"],
+    ["Institute Type", "Government"],
+    ["Established", "2016"],
+    ["Location", "Ranchi, Jharkhand"],
+    ["Type", "IIIT"]
+];
+
+export const IIITRanchiAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Ranchi"
+];
+
+export const IIITUnaAboutDataTableConstant = [
+    ["Also Known as", "IIITU"],
+    ["Institute Type", "Government"],
+    ["Established", "2014"],
+    ["Location", "Una, Himachal Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITUnaAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Una"
+];
+
+export const IIITSuratAboutDataTableConstant = [
+    ["Also Known as", "IIITS"],
+    ["Institute Type", "Government"],
+    ["Established", "2017"],
+    ["Location", "Surat, Gujarat"],
+    ["Type", "IIIT"]
+];
+
+export const IIITSuratAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Surat"
+];
+
+export const IIITManipurAboutDataTableConstant = [
+    ["Also Known as", "IIITM"],
+    ["Institute Type", "Government"],
+    ["Established", "2015"],
+    ["Location", "Imphal, Manipur"],
+    ["Type", "IIIT"]
+];
+
+export const IIITManipurAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Manipur"
+];
+
+export const IIITKurnoolAboutDataTableConstant = [
+    ["Also Known as", "IIITK"],
+    ["Institute Type", "Government"],
+    ["Established", "2015"],
+    ["Location", "Kurnool, Andhra Pradesh"],
+    ["Type", "IIIT"]
+];
+
+export const IIITKurnoolAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Kurnool"
+];
+
+export const IIITTiruchirapalliAboutDataTableConstant = [
+    ["Also Known as", "IIITT"],
+    ["Institute Type", "Government"],
+    ["Established", "2013"],
+    ["Location", "Tiruchirapalli, Tamil Nadu"],
+    ["Type", "IIIT"]
+];
+
+export const IIITTiruchirapalliAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Tiruchirapalli"
+];
+
+export const IIITSonepatAboutDataTableConstant = [
+    ["Also Known as", "IIITS"],
+    ["Institute Type", "Government"],
+    ["Established", "2014"],
+    ["Location", "Sonepat, Haryana"],
+    ["Type", "IIIT"]
+];
+
+export const IIITSonepatAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Sonepat"
+];
+
+export const IIITAgartalaAboutDataTableConstant = [
+    ["Also Known as", "IIITA"],
+    ["Institute Type", "Government"],
+    ["Established", "2018"],
+    ["Location", "Agartala, Tripura"],
+    ["Type", "IIIT"]
+];
+
+export const IIITAgartalaAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Agartala"
+];
+
+export const IIITKarnatakaAboutDataTableConstant = [
+    ["Also Known as", "IIITK"],
+    ["Institute Type", "Government"],
+    ["Established", "2015"],
+    ["Location", "Raichur, Karnataka"],
+    ["Type", "IIIT"]
+];
+
+export const IIITKarnatakaAboutDataTableHeading = [
+    "Institute Name",
+    "Indian Institute of Information Technology, Karnataka"
+];
