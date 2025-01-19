f53c5433a021b2c7a88de6e27cb9ea7800102c52
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-19 22:56:10 +0530

    Fix the carousel issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import "./index.css"
+// import './styles/tailwind.css'; // Import Tailwind CSS
 
+// import "./style.css"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,23 @@ import flowbite from "flowbite-react/tailwind";
 
 export default {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-
+  
   content: [
     "./index.html", 
     "./src/**/*.{js,ts,jsx,tsx}", 
     "./node_modules/flowbite/**/*.js"],
+
+    safelist: [
+      'bg-blue-forComponentBg', // Add any other classes you want to safelist
+      'hover:bg-blue-800',
+      'focus:ring-4',
+      'focus:outline-none',
+      'focus:ring-blue-300',
+      'dark:bg-blue-600',
+      'dark:hover:bg-blue-700',
+      'dark:focus:ring-blue-800',
+      // Add more classes as needed
+    ],
   theme: {
     extend: {
       colors: {
