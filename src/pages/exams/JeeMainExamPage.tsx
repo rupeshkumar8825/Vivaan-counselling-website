e39cd06a9e38c4254ea93fcd4b7824fac8356984
Rupesh Kumar <therupeshkumar8825@gmail.com>
2025-01-19 21:24:44 +0530

    Fix the build warnings and errors

--- src/pages/exams/JeeMainExamPage.tsx
+++ src/pages/exams/JeeMainExamPage.tsx
@@ -5,7 +5,7 @@ import BlogComponent from "../../components/blog/BlogComponent"
 import { JeeMainContentList, JeeMainExamPageButtonCodesList, JeeMainExamPageButtonWidthList, JeeMainExamsButtonNameList, JeeMainHeadingIdList, JeeMainImageList, JeeMainsExamRoutesList, JeeMainsExamSubMenuList, JeeMainsHeadingList, JeeMainTableOfContentList, JeeMainVideoCardTitleList, JeeMainVideoCardUrlList } from "../../constants/exam/JeeMainsExamPageConstants"
 import { useNavigate } from "react-router-dom"
 import TopBanner from "../../home/components/TopBanner"
-import FAQComponent from "../../components/common/FAQComponent"
+// import FAQComponent from "../../components/common/FAQComponent"
 
 const JeeMainExamPage = () => {
     const navigate = useNavigate()
